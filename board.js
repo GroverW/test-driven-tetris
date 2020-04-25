6754a86cfa5164ef729fad160dd17b06ea61b9b8
GroverW <cheesemaker@gmail.com>
2020-04-25 16:16:47 -0700

    added rotate piece to board

--- board.js
+++ board.js
@@ -8,6 +8,22 @@ class Board {
   createEmptyGrid() {
     return new Array(BOARD_WIDTH).map(() => Array(BOARD_HEIGHT).fill(0));
   }
+
+  rotatePiece(piece, direction) {
+    let newGrid = JSON.parse(JSON.stringify(piece.grid));
+
+    for(let i = 0; i < newGrid.length; i++) {
+      for(let j = 0; j < i; j++) {
+        [newGrid[j][i], newGrid[i][j]] = [newGrid[i][j], newGrid[j][i]];
+      }
+    }
+
+    direction > 0
+      ? newGrid.forEach(row => row.reverse())
+      : newGrid.reverse();
+
+    piece.update(newGrid);
+  }
 }
 
 module.exports = Board;
