241ebdad37e3f1b9188e5231a7869b8c246131e9
GroverW <cheesemaker@gmail.com>
2020-04-25 16:17:13 -0700

    added ROTATE left/right and changed L/J pieces

--- data.js
+++ data.js
@@ -1,5 +1,7 @@
 const BOARD_WIDTH = 10;
 const BOARD_HEIGHT = 20;
+const ROTATE_LEFT = -1;
+const ROTATE_RIGHT = 1;
 const PIECES = [
   [
     [0,0,0,0],
@@ -27,13 +29,13 @@ const PIECES = [
     [0,0,0]
   ],
   [
-    [6,0,0],
     [6,6,6],
+    [6,0,0],
     [0,0,0]
   ],
   [
-    [0,0,7],
     [7,7,7],
+    [0,0,7],
     [0,0,0]
   ]
 ]
@@ -42,6 +44,8 @@ const SEED_PIECES = [0,0,0,0,1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6]
 module.exports = {
   BOARD_WIDTH,
   BOARD_HEIGHT,
+  ROTATE_LEFT,
+  ROTATE_RIGHT,
   PIECES,
   SEED_PIECES
 }
