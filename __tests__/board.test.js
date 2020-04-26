b0a5820ea97fd86d40d3f1eeb4588d83ab410845
GroverW <cheesemaker@gmail.com>
2020-04-26 12:34:28 -0700

    added drop tests - drop piece if invalid move down - clears single line - clears multiple lines - clears non-consecutive lines

--- __tests__/board.test.js
+++ __tests__/board.test.js
@@ -6,7 +6,7 @@ const { TEST_BOARDS } = require('./helpers/testData');
 
 describe('game board tests', () => {
   let gameBoard;
-  let p1, p2, p3, p4;
+  let p1, p2, p3, p4, p5;
   
   beforeEach(() => {
     gameBoard = new Board();
@@ -14,6 +14,7 @@ describe('game board tests', () => {
     p2 = new Piece(PIECES[1]);
     p3 = new Piece(PIECES[2]);
     p4 = new Piece(PIECES[5]);
+    p5 = new Piece(PIECES[6]);
   });
   
   test('creates a new, empty board', () => {
@@ -89,6 +90,61 @@ describe('game board tests', () => {
     gameBoard.hardDrop();
 
     expect(gameBoard.grid).toEqual(TEST_BOARDS.pattern5);
+  });
+
+  test('drop piece if invalid move down', () => {
+    gameBoard.piece = p1;
+    gameBoard.movePiece(0,18);
+    
+    expect(gameBoard.grid).toEqual(TEST_BOARDS.empty);
+    expect([p1.x, p1.y]).toEqual([3,18]);
+
+    gameBoard.movePiece(0,1);
+
+    expect(gameBoard.grid).toEqual(TEST_BOARDS.pattern1);
+    expect([p1.x, p1.y]).toEqual([3,18]);
+
+    // don't drop piece if invalid horizontal move
+    gameBoard.piece = p2;
+    gameBoard.movePiece(-4,0);
+
+    expect(gameBoard.grid).toEqual(TEST_BOARDS.pattern1);
+    expect([p2.x, p2.y]).toEqual([0,0]);
+
+    gameBoard.movePiece(-1,0);
+
+    expect(gameBoard.grid).toEqual(TEST_BOARDS.pattern1);
+    expect([p2.x, p2.y]).toEqual([0,0]);
+  });
+
+  test('clears single line', () => {
+    gameBoard.grid = JSON.parse(JSON.stringify(TEST_BOARDS.clearLines1));
+    gameBoard.piece = p1;
+
+    gameBoard.hardDrop();
+
+    expect(gameBoard.grid).toEqual(TEST_BOARDS.clearLines1Cleared);
+  });
+
+  test('clears multiple lines', () => {
+    gameBoard.grid = JSON.parse(JSON.stringify(TEST_BOARDS.clearLines2));
+    gameBoard.piece = p1;
+    
+    gameBoard.rotatePiece(gameBoard.piece, ROTATE_LEFT);
+    gameBoard.hardDrop();
+
+    expect(gameBoard.grid).toEqual(TEST_BOARDS.clearLines2Cleared);
+  });
+
+  test('clears lines from the middle of the board', () => {
+    gameBoard.grid = JSON.parse(JSON.stringify(TEST_BOARDS.clearLines3));
+    gameBoard.piece = p5;
+    
+    gameBoard.rotatePiece(gameBoard.piece, ROTATE_LEFT);
+    gameBoard.movePiece(4,0);
+    gameBoard.hardDrop();
+
+    expect(gameBoard.grid).toEqual(TEST_BOARDS.clearLines3Cleared);
   });
 })
 
