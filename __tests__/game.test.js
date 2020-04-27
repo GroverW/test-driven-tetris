be49d8eb99a91a999a1e98d7b50360fe843f8b88
GroverW <cheesemaker@gmail.com>
2020-04-26 18:21:23 -0700

    added game tests for game start and commands

---
+++ __tests__/game.test.js
@@ -0,0 +1,75 @@
+const Game = require('../game');
+const Board = require('../board');
+const { Piece } = require ('../piece');
+const { PIECES, CONTROLS } = require('../data');
+const { TEST_BOARDS } = require('./helpers/testData');
+
+describe('game tests', () => {
+  let game;
+  let p1;
+
+  beforeEach(() => {
+    game = new Game();
+    p1 = new Piece(PIECES[0]);
+  })
+
+  test('start game', () => {
+    expect([game.score, game.level, game.lines]).toEqual([0,1,0]);
+    expect(game.board.grid).toEqual(TEST_BOARDS.empty);
+    
+    expect(game.board.piece).not.toEqual(expect.any(Piece));
+    expect(game.board.nextPiece).not.toEqual(expect.any(Piece));
+
+    game.start();
+
+    expect(game.board.piece).toEqual(expect.any(Piece));
+    expect(game.board.nextPiece).toEqual(expect.any(Piece));
+  });
+
+  test('keyboard controls', () => {
+    game.board.piece = p1;
+
+    expect([p1.x, p1.y]).toEqual([3,0]);
+
+    game.command(CONTROLS.DOWN);
+
+    expect([p1.x, p1.y]).toEqual([3,1]);
+
+    game.command(CONTROLS.LEFT);
+
+    expect([p1.x, p1.y]).toEqual([2,1]);
+
+    game.command(CONTROLS.RIGHT);
+
+    expect([p1.x, p1.y]).toEqual([3,1]);    
+
+    expect(game.board.piece.grid).toEqual([
+      [0,0,0,0],
+      [1,1,1,1],
+      [0,0,0,0],
+      [0,0,0,0],
+    ])
+
+    game.command(CONTROLS.ROTATE_LEFT);
+
+    expect(game.board.piece.grid).toEqual([
+      [0,1,0,0],
+      [0,1,0,0],
+      [0,1,0,0],
+      [0,1,0,0],
+    ])
+
+    game.command(CONTROLS.ROTATE_RIGHT);
+
+    expect(game.board.piece.grid).toEqual([
+      [0,0,0,0],
+      [1,1,1,1],
+      [0,0,0,0],
+      [0,0,0,0],
+    ])
+
+    game.command(CONTROLS.HARD_DROP);
+
+    expect(game.board.grid).toEqual(TEST_BOARDS.pattern1);
+  })
+});
