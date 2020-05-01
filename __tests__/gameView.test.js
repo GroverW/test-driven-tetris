774240a42977aab0174a861f0f03e3eb2e060d62
GroverW <cheesemaker@gmail.com>
2020-04-30 17:15:18 -0700

    starting tests for gameView

---
+++ __tests__/gameView.test.js
@@ -0,0 +1,40 @@
+const GameView = require('../gameView')
+const Game = require('../game');
+const { Piece } = require ('../piece');
+const { PIECES, CONTROLS,  } = require('../data');
+const { TEST_BOARDS } = require('./helpers/testData');
+const { publish } = require('../pubSub');
+
+describe('game view tests', () => {
+  let game;
+  let p1, p2, p3;
+  let mockCtx
+
+  beforeEach(() => {
+    mockCtx = {
+      fillStyle,
+      strokeStyle,
+      rect: jest.fn()
+    }
+
+    game = new Game();
+    gameView = new GameView(mockCtx);
+    p1 = new Piece(PIECES[0]);
+    p2 = new Piece(PIECES[6]);
+    p3 = new Piece(PIECES[2]);
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    game.unsubDrop();
+    game.unsubClear();
+    game.unsubGame();
+    gameView.unsubDraw();
+  });
+
+  test('draw board', () => {
+    game.start();
+
+    expect(mockCtx.fillRect).toHaveBeenCalled();
+  })
+});
