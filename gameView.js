8d591a7e89f4da43f235f04b4c76e8df06b9101b
GroverW <cheesemaker@gmail.com>
2020-04-30 18:04:27 -0700

    added drawAll and drawElement to GameView

--- gameView.js
+++ gameView.js
@@ -5,11 +5,16 @@ class GameView {
   constructor(ctx) {
     this.ctx = ctx;
     this.ctx.strokeStyle = "black";
-    this.unsubDraw = subscribe('drawAll', this.drawBoard.bind(this));
+    this.unsubDraw = subscribe('drawAll', this.drawAll.bind(this));
   }
 
-  drawBoard(board) {
-    board.grid.forEach((row, rowIdx) => row.forEach((cell, colIdx) => {
+  drawAll(data) {
+    this.drawElement(data.piece);
+    this.drawElement(data.board);
+  }
+
+  drawElement(element) {
+    element.grid.forEach((row, rowIdx) => row.forEach((cell, colIdx) => {
       this.ctx.fillStyle = CELL_COLORS[cell];
       this.ctx.rect(colIdx, rowIdx, 1, 1);
     }))
