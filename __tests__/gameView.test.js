4a0667197308afc4fdcde9e36f393efcd774f47e
GroverW <cheesemaker@gmail.com>
2020-04-30 18:59:35 -0700

    added tests for when nextPiece should be drawn

--- __tests__/gameView.test.js
+++ __tests__/gameView.test.js
@@ -18,7 +18,7 @@ describe('game view tests', () => {
     }
 
     game = new Game();
-    gameView = new GameView(mockCtx);
+    gameView = new GameView(mockCtx, mockCtx);
     p1 = new Piece(PIECES[0]);
     p2 = new Piece(PIECES[6]);
     p3 = new Piece(PIECES[2]);
@@ -33,10 +33,33 @@ describe('game view tests', () => {
   });
 
   test('draw elements on game start', () => {
-    const drawELementSpy = jest.spyOn(gameView, 'drawElement');
+    const drawBoardSpy = jest.spyOn(gameView, 'drawBoard');
+    const drawPieceSpy = jest.spyOn(gameView, 'drawPiece');
 
     game.start();
     
-    expect(drawELementSpy).toHaveBeenCalledTimes(2);
-  })
+    expect(drawBoardSpy).toHaveBeenCalled();
+    expect(drawPieceSpy).toHaveBeenCalledTimes(2);
+  });
+
+  test('only draw board and piece until piece dropped', () => {
+    game.start();
+    
+    const drawBoardSpy = jest.spyOn(gameView, 'drawBoard');
+    const drawPieceSpy = jest.spyOn(gameView, 'drawPiece');
+
+    expect(drawBoardSpy).not.toHaveBeenCalled();
+    expect(drawPieceSpy).not.toHaveBeenCalled();
+
+    game.command(CONTROLS.DOWN);
+
+    expect(drawBoardSpy).toHaveBeenCalled();
+    expect(drawPieceSpy).toHaveBeenCalledTimes(1);
+
+    game.command(CONTROLS.HARD_DROP);
+
+    expect(drawBoardSpy).toHaveBeenCalledTimes(2);
+    expect(drawPieceSpy).toHaveBeenCalledTimes(3);
+  });
+
 });
