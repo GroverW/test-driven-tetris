c049585d6228257cc58bd25f5be80f2f46c61c2e
GroverW <cheesemaker@gmail.com>
2020-05-01 13:30:27 -0700

    added basic ws and .get routes to app

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 
 const express = require('express');
 const app = express();
+// const server = require('http').Server(app)
+// const io = require('socket.io')(server);
+const wsExpress = require('express-ws')(app);
 
 // serve stuff in static/ folder
 app.use(express.static('static/'));
@@ -9,4 +12,21 @@ app.use(express.static('static/'));
 /**Handle websocket messages */
 
 //allow for app.ws routes for websocket routes
-const wsExpress = require('express-ws')(app);
+app.ws('/test', (ws, req, next) => {
+
+
+  ws.on('connection', () => {
+    console.log("CONNECTED!")
+    ws.send('you did it');
+  });
+
+  ws.on('message', msg => {
+    ws.send(msg);
+  });
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+module.exports = app;
