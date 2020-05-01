ad18a50d6d913a92bb5ff35154ba0b2a4e729e21
GroverW <cheesemaker@gmail.com>
2020-05-01 13:27:43 -0700

    added test for getNewPlayer in utils.test.js

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,19 +1,27 @@
-const { randomize } = require('../helpers/utils');
+const { randomize, getNewPlayer } = require('../helpers/utils');
 
 describe('utils', () => {
   let pieces = [1,2,3,4,5,6,7];
 
   beforeAll(() => {
     Math.random = jest.fn().mockReturnValue(.5)
-  })
+  });
 
   afterAll(() => {
     jest.clearAllMocks();
-  })
+  });
 
   test('randomize pieces', () => {
     expect(randomize(pieces)).toEqual([1,6,2,5,3,7,4]);
-  })
-});
+  });
 
-[]
+  test('get new player', () => {
+    const testPlayer = {
+      ctx: 'test',
+      board: [],
+      id: 1
+    }
+
+    expect(getNewPlayer('test', [], 1)).toEqual(testPlayer)
+  });
+});
