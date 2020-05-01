2f4016431d7a3e10eb758db2839db7f668f54f35
GroverW <cheesemaker@gmail.com>
2020-05-01 13:33:52 -0700

    added tests and refactored gameView.test.js - removed Piece, PIECES, TEST_BOARDS, publish - added BOARD_HEIGHT, BOARD_WIDTH, CELL_SIZE, getMockCtx, getTestBoard - moved test data to beforeEach setup - added test for addNewPlayer - added test for making sure addNewPlayer resizes player 2 board when a third player joins - added test for removePlayer - added test for making sure removePlayer resizes player 2 board when a third player leaves

--- __tests__/gameView.test.js
+++ __tests__/gameView.test.js
@@ -1,27 +1,35 @@
-const GameView = require('../gameView')
-const Game = require('../game');
-const { Piece } = require ('../piece');
-const { PIECES, CONTROLS,  } = require('../data');
-const { TEST_BOARDS } = require('./helpers/testData');
-const { publish } = require('../pubSub');
+const GameView = require('../static/js/gameView')
+const Game = require('../static/js/game');
+const { 
+  CONTROLS,
+  BOARD_HEIGHT,
+  BOARD_WIDTH,
+  CELL_SIZE 
+} = require('../static/js/data');
+const { getMockCtx, getTestBoard } = require('./helpers/testData');
 
 describe('game view tests', () => {
   let game;
-  let p1, p2, p3;
-  let mockCtx
+  let mockCtx, mockCtxNext;
+  let newCtx1, newBoard1, newId1;
+  let newCtx2, newBoard2, newId2;
+  let drawBoardSpy, drawPieceSpy;
 
   beforeEach(() => {
-    mockCtx = {
-      fillStyle: "",
-      strokeStyle: "",
-      rect: jest.fn()
-    }
+    mockCtx = getMockCtx();
+    mockCtxNext = getMockCtx();
+    newCtx1 = getMockCtx();
+    newBoard1 = getTestBoard('empty');
+    newId1 = 1;
+    newCtx2 = getMockCtx();
+    newBoard2 = getTestBoard('empty');
+    newId2 = 2;
 
     game = new Game();
-    gameView = new GameView(mockCtx, mockCtx);
-    p1 = new Piece(PIECES[0]);
-    p2 = new Piece(PIECES[6]);
-    p3 = new Piece(PIECES[2]);
+    gameView = new GameView(mockCtx, mockCtxNext);
+
+    drawBoardSpy = jest.spyOn(gameView, 'drawBoard');
+    drawPieceSpy = jest.spyOn(gameView, 'drawPiece');
   })
 
   afterEach(() => {
@@ -33,33 +41,130 @@ describe('game view tests', () => {
   });
 
   test('draw elements on game start', () => {
-    const drawBoardSpy = jest.spyOn(gameView, 'drawBoard');
-    const drawPieceSpy = jest.spyOn(gameView, 'drawPiece');
+    expect(gameView.ctx.canvas.width).toBe(BOARD_WIDTH * CELL_SIZE);
+    expect(gameView.ctx.canvas.height).toBe(BOARD_HEIGHT * CELL_SIZE);
+    expect(gameView.ctxNext.canvas.width).toBe(4 * CELL_SIZE);
+    expect(gameView.ctxNext.canvas.height).toBe(4 * CELL_SIZE);
 
     game.start();
-    
+
     expect(drawBoardSpy).toHaveBeenCalled();
     expect(drawPieceSpy).toHaveBeenCalledTimes(2);
   });
 
-  test('only draw board and piece until piece dropped', () => {
+  test('only draw nextPiece when piece dropped', () => {
     game.start();
-    
-    const drawBoardSpy = jest.spyOn(gameView, 'drawBoard');
-    const drawPieceSpy = jest.spyOn(gameView, 'drawPiece');
 
-    expect(drawBoardSpy).not.toHaveBeenCalled();
-    expect(drawPieceSpy).not.toHaveBeenCalled();
+    expect(drawBoardSpy).toHaveBeenCalledTimes(1);
+    expect(drawPieceSpy).toHaveBeenCalledTimes(2);
 
     game.command(CONTROLS.DOWN);
 
-    expect(drawBoardSpy).toHaveBeenCalled();
-    expect(drawPieceSpy).toHaveBeenCalledTimes(1);
+    // redraw board and piece once per command
+    expect(drawBoardSpy).toHaveBeenCalledTimes(2);
+    expect(drawPieceSpy).toHaveBeenCalledTimes(3);
 
     game.command(CONTROLS.HARD_DROP);
 
-    expect(drawBoardSpy).toHaveBeenCalledTimes(2);
-    expect(drawPieceSpy).toHaveBeenCalledTimes(3);
+    // when a new piece is grabbed, board, piece and nextPiece
+    // should be drawn
+    expect(drawBoardSpy).toHaveBeenCalledTimes(3);
+    expect(drawPieceSpy).toHaveBeenCalledTimes(5);
   });
 
+  test('add new player', () => {
+    game.start();
+    
+    expect(gameView.players.length).toBe(0);
+
+    expect(drawBoardSpy).toHaveBeenCalledTimes(1);
+    expect(drawPieceSpy).toHaveBeenCalledTimes(2);
+
+    gameView.addPlayer(newCtx1, newBoard1);
+
+    expect(gameView.players.length).toBe(1);
+
+    expect(drawBoardSpy).toHaveBeenCalledTimes(2);
+    expect(drawPieceSpy).toHaveBeenCalledTimes(2);
+  });
+
+  test('adding 3rd player should reduce board size of 2nd', () => {
+    game.start();
+    const fullBoardWidth = BOARD_WIDTH * CELL_SIZE;
+    const fullBoardHeight = BOARD_HEIGHT * CELL_SIZE;
+    const halfBoardWidth = BOARD_WIDTH * CELL_SIZE / 2;
+    const halfBoardHeight = BOARD_HEIGHT * CELL_SIZE / 2;
+    const fullCell = CELL_SIZE;
+    const halfCell = CELL_SIZE / 2;
+
+    expect(gameView.players.length).toBe(0);
+    expect(drawBoardSpy).toHaveBeenCalledTimes(1);
+    
+    
+    gameView.addPlayer(newCtx1, newBoard1)
+    
+    expect(gameView.players.length).toBe(1);
+    expect(drawBoardSpy).toHaveBeenCalledTimes(2);
+
+    expect(gameView.players[0].ctx.canvas.width).toBe(fullBoardWidth);
+    expect(gameView.players[0].ctx.canvas.height).toBe(fullBoardHeight);
+    expect(gameView.players[0].ctx.canvas.xScale).toBe(fullCell);
+    expect(gameView.players[0].ctx.canvas.yScale).toBe(fullCell);
+    
+    gameView.addPlayer(newCtx2, newBoard2);
+
+    expect(gameView.players.length).toBe(2);
+    expect(drawBoardSpy).toHaveBeenCalledTimes(4);
+
+    expect(gameView.players[0].ctx.canvas.width).toBe(halfBoardWidth);
+    expect(gameView.players[0].ctx.canvas.height).toBe(halfBoardHeight);
+    expect(gameView.players[0].ctx.canvas.xScale).toBe(halfCell);
+    expect(gameView.players[0].ctx.canvas.yScale).toBe(halfCell);
+    expect(gameView.players[1].ctx.canvas.width).toBe(halfBoardWidth);
+    expect(gameView.players[1].ctx.canvas.height).toBe(halfBoardHeight);
+    expect(gameView.players[1].ctx.canvas.xScale).toBe(halfCell);
+    expect(gameView.players[1].ctx.canvas.yScale).toBe(halfCell);
+
+  })
+
+  test('remove player', () => {
+    game.start();
+    
+    expect(gameView.players.length).toBe(0);
+
+    gameView.addPlayer(newCtx1, newBoard1, newId1)
+    gameView.addPlayer(newCtx2, newBoard2, newId2)
+
+    expect(gameView.players.length).toBe(2);
+
+    gameView.removePlayer(newId1);
+
+    expect(gameView.players.length).toBe(1);
+    expect(gameView.players[0].id).toBe(2)
+  });
+
+  test('removing down to 2 players rescales player 2 board', () => {
+    game.start();
+    const fullBoardWidth = BOARD_WIDTH * CELL_SIZE;
+    const fullBoardHeight = BOARD_HEIGHT * CELL_SIZE;
+    const halfBoardWidth = BOARD_WIDTH * CELL_SIZE / 2;
+    const halfBoardHeight = BOARD_HEIGHT * CELL_SIZE / 2;
+    
+    expect(gameView.players.length).toBe(0);
+
+    gameView.addPlayer(newCtx1, newBoard1, newId1)
+    gameView.addPlayer(newCtx2, newBoard2, newId2)
+
+    expect(gameView.players.length).toBe(2);
+    expect(gameView.players[0].ctx.canvas.width).toBe(halfBoardWidth);
+    expect(gameView.players[0].ctx.canvas.height).toBe(halfBoardHeight);
+    expect(gameView.players[1].ctx.canvas.width).toBe(halfBoardWidth);
+    expect(gameView.players[1].ctx.canvas.height).toBe(halfBoardHeight);
+
+    gameView.removePlayer(newId1);
+
+    expect(gameView.players.length).toBe(1);
+    expect(gameView.players[0].ctx.canvas.width).toBe(fullBoardWidth);
+    expect(gameView.players[0].ctx.canvas.height).toBe(fullBoardHeight);
+  });
 });
