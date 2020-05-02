8e5a8de53fd621c1880f6ecf7a2d0c26d3647278
GroverW <cheesemaker@gmail.com>
2020-05-02 15:42:15 -0700

    added test data, join and leeave room tests for gameServer

--- src/__tests__/gameServer.test.js
+++ src/__tests__/gameServer.test.js
@@ -4,8 +4,14 @@ const Player = require('../js/player');
 describe('game manager tests', () => {
   let gameServer;
   let gameServerId;
+  let p1, p2, p3, p4, p5;
 
   beforeEach(() => {
+    p1 = new Player();
+    p2 = new Player();
+    p3 = new Player();
+    p4 = new Player();
+    p5 = new Player();
     gameServer = new GameServer();
     gameServerId = 1;
   })
@@ -17,16 +23,30 @@ describe('game manager tests', () => {
   });
 
   test('join game', () => {
-    const player = new Player();
-
-    gameServer.join(player);
-
-    expect(gameServer.players.length).toBe(1);
+    expect(gameServer.join(p1)).toBe(true);
+    expect(gameServer.players.size).toBe(1);
 
   });
 
-  test('leave game', () => {
+  test('join game - game full', () => {
+    expect(gameServer.join(p1)).toBe(true);
+    expect(gameServer.join(p2)).toBe(true);
+    expect(gameServer.join(p3)).toBe(true);
+    expect(gameServer.join(p4)).toBe(true);
+    
+    expect(gameServer.players.size).toBe(4);
+    
+    expect(gameServer.join(p5)).toBe(false);
 
+    expect(gameServer.players.size).toBe(4);
+  });
+
+  test('leave game', () => {
+    gameServer.join(p1);
+    expect(gameServer.players.size).toBe(1);
+
+    gameServer.leave(p1);
+    expect(gameServer.players.size).toBe(0);
   });
 
   test('broadcast', () => {
