70f18c433f46b82cc0724ded2a52eda6a5e66ba7
GroverW <cheesemaker@gmail.com>
2020-05-02 17:14:08 -0700

    added data from frontend to data helper

--- src/helpers/data.js
+++ src/helpers/data.js
@@ -1,7 +1,70 @@
 const GAMES = new Map();
 const MAX_PLAYERS = 4;
 
+const LINES_PER_LEVEL = 10;
+const ROTATE_LEFT = -1;
+const ROTATE_RIGHT = 1;
+const CONTROLS = {
+  LEFT: 37,
+  RIGHT: 39,
+  DOWN: 40,
+  ROTATE_LEFT: 65,
+  ROTATE_RIGHT: 83,
+  HARD_DROP: 32
+};
+
+const POINTS = {
+  DOWN: 1,
+  HARD_DROP: 2,
+  LINES_CLEARED: {
+    1: 100,
+    2: 300,
+    3: 500,
+    4: 800
+  }
+};
+
+const PIECES = [[
+    [0,0,0,0],
+    [1,1,1,1],
+    [0,0,0,0],
+    [0,0,0,0]
+  ], [
+    [2,2],
+    [2,2]
+  ], [
+    [0,3,0],
+    [3,3,3],
+    [0,0,0]
+  ], [
+    [0,4,4],
+    [4,4,0],
+    [0,0,0]
+  ], [
+    [5,5,0],
+    [0,5,5],
+    [0,0,0]
+  ], [
+    [6,6,6],
+    [6,0,0],
+    [0,0,0]
+  ], [
+    [7,7,7],
+    [0,0,7],
+    [0,0,0]
+  ]
+]
+const SEED_PIECES = [0,0,0,0,1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6];
+
+
 module.exports = {
   GAMES,
-  MAX_PLAYERS
+  MAX_PLAYERS,
+  LINES_PER_LEVEL,
+  ROTATE_LEFT,
+  ROTATE_RIGHT,
+  CONTROLS,
+  POINTS,
+  PIECES,
+  SEED_PIECES
 }
