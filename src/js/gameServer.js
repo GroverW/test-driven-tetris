ff3255eb15866a104af40030d5501b1ef1e576b0
GroverW <cheesemaker@gmail.com>
2020-05-02 23:13:19 -0700

    added properties and methods to gameServer class - now has id so it can be removed from GAMES - added subscriptions to each player pubsub - added removesubscriptions to each player pubsub - first player to join is now host - when host leave, next player becomes host - when game is empty, gets deleted from GAMES - get pieces now creates a new set and sends it to each player

--- src/js/gameServer.js
+++ src/js/gameServer.js
@@ -1,42 +1,66 @@
-const { GAMES, MAX_PLAYERS } = require('../helpers/data');
+const { GAMES, MAX_PLAYERS, RANKINGS, SEED_PIECES } = require('../helpers/data');
+const { randomize } = require('../helpers/utils');
 
 class GameServer {
-  constructor() {
+  constructor(id) {
+    this.id = id;
     this.players = new Set();
     this.gameStarted = false;
     this.nextRanking;
+    this.subscriptions = {};
   }
 
   static get(id) {
-    if(!GAMES.has(id)) GAMES.set(id, new GameServer())
+    if (!GAMES.has(id)) GAMES.set(id, new GameServer(id))
 
     return GAMES.get(id);
   }
 
   join(player) {
-    if(this.players.size < MAX_PLAYERS && !this.gameStarted) {
+    if (this.players.size < MAX_PLAYERS && !this.gameStarted) {
       this.players.add(player);
-      player.init(this);
       
+      this.addSubscriptions(player)
+
+      if (this.players.size === 1) player.isHost = true;
+
       this.sendAllExcept(player, {
         message: 'addPlayer',
         data: player.id
       });
-      
+
       return true;
     }
-    
+
     return false;
+  }
+
+  addSubscriptions(player) {
+    this.subscriptions[player.id] = [
+      player.pubSub.subscribe('leave', this.leave.bind(this)),
+      player.pubSub.subscribe('startGame', this.startGame.bind(this)),
+      player.pubSub.subscribe('getRanking', this.gameOver.bind(this)),
+      player.pubSub.subscribe('getPieces', this.getPieces.bind(this)),
+    ]
+  }
+
+  removeSubscriptions(id) {
+    this.subscriptions[id].forEach(unsub => unsub());
   }
 
   leave(player) {
-    if(this.players.has(player)) {
+    if (this.players.has(player)) {
+      if (player.isHost && this.players.size > 1) this.setNewHost()
+
+      this.removeSubscriptions(player.id);
       this.players.delete(player);
 
-      this.sendAllExcept(player, {
-        message: 'removePlayer',
-        data: player.id
-      });
+      this.players.size === 0
+        ? GAMES.delete(this.id)
+        : this.sendAllExcept(player, {
+            message: 'removePlayer',
+            data: player.id
+          });
 
       return true;
     }
@@ -44,28 +68,48 @@ class GameServer {
     return false;
   }
 
+  setNewHost() {
+    const playersIterator = this.players.values();
+    playersIterator.next()
+    const newHost = playersIterator.next().value;
+    newHost.isHost = true;
+  }
+
   sendAll(data) {
-    for(let player of this.players) {
-      player._send(JSON.stringify(data));
-    }
+    this.players.forEach(player => player._send(JSON.stringify(data)));
   }
 
   sendAllExcept(exceptPlayer, data) {
-    for(let player of this.players) {
-      (player !== exceptPlayer) && player._send(JSON.stringify(data));
+    this.players.forEach(player =>
+      (player !== exceptPlayer) && player._send(JSON.stringify(data))
+    );
+  }
+
+  startGame(player) {
+    if(player && player.isHost) {
+      this.gameStarted = true;
+
+      this.getPieces();
+      
+      this.sendAll({
+        message: 'startGame',
+      });
+
+      this.nextRanking = this.players.size;
     }
   }
 
-  sendTo(player, data) {
-    player._send(JSON.stringify(data));
-  }
+  getPieces() {
+    const pieces = randomize(SEED_PIECES);
 
-  startGame() {
-    this.gameStarted = true;
-    this.sendAll({
-      message: 'startGame',
+    this.players.forEach(player => {
+      player.game.board.pieceList.addSet(pieces);
+      
+      player._send(JSON.stringify({
+        message: 'addPieces',
+        data: pieces
+      }))
     })
-    this.nextRanking = this.players.size;
   }
 
   gameOver(player) {
@@ -77,10 +121,10 @@ class GameServer {
       }
     });
 
-    this.sendTo(player, {
+    player._send(JSON.stringify({
       message: 'gameOver',
-      data: this.nextRanking
-    });
+      data: RANKINGS[this.nextRanking]
+    }))
 
     this.nextRanking--;
   }
