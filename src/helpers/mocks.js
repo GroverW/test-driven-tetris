5a3bf32d2708e64bbbfb1a550e7ed34ad15824d4
GroverW <cheesemaker@gmail.com>
2020-05-02 17:45:07 -0700

    added TEST_BOARDS and getTestBoard to mocks

--- src/helpers/mocks.js
+++ src/helpers/mocks.js
@@ -1,5 +1,12 @@
+const TEST_BOARDS = require('./sampleBoards');
+
 const mockSend = () => {};
+
+const getTestBoard = (board) =>
+  JSON.parse(JSON.stringify(TEST_BOARDS[board]));
 
 module.exports = {
   mockSend,
+  TEST_BOARDS,
+  getTestBoard
 }
