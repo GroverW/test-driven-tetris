73999a2504dc70c976b036150b75fe0e1924ae5e
GroverW <cheesemaker@gmail.com>
2020-05-03 09:43:36 -0700

    added getTestPieces to mocks

--- src/helpers/mocks.js
+++ src/helpers/mocks.js
@@ -1,12 +1,17 @@
 const TEST_BOARDS = require('./sampleBoards');
+const { randomize } = require('./utils');
+const { SEED_PIECES } = require('./data');
 
 const mockSend = () => {};
 
 const getTestBoard = (board) =>
   JSON.parse(JSON.stringify(TEST_BOARDS[board]));
 
+const getTestPieces = () => randomize(SEED_PIECES);
+
 module.exports = {
   mockSend,
   TEST_BOARDS,
-  getTestBoard
+  getTestBoard,
+  getTestPieces
 }
