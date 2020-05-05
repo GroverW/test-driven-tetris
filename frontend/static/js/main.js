7afca98274061eae639e0f042d9ac4e0800bc464
GroverW <cheesemaker@gmail.com>
2020-05-04 20:50:46 -0700

    added basic ws commands to main.js

--- frontend/static/js/main.js
+++ frontend/static/js/main.js
@@ -1,12 +1,10 @@
 const Game = require('./game');
 const GameDOM = require('./gameDOM');
 const { publish } = require('../../helpers/pubSub');
+const Api = require('../../helpers/api');
 
 const p1Canvas = document.getElementById('p1-board');
 const nextPieceCanvas = document.getElementById('next-piece');
-
-const game = new Game();
-
 const selectors = {
   playerCtx: p1Canvas.getContext('2d'),
   nextCtx: nextPieceCanvas.getContext('2d'),
@@ -16,13 +14,56 @@ const selectors = {
   linesSelector: document.getElementById('game-lines'),
 }
 
-const view = new GameDOM(selectors);
-game.start();
+const gameId = 1;
+const ws = new WebSocket(`ws://localhost:3000/game/${gameId}`);
 
-let i = 1;
-publish('addPlayer', i);
+let game, gameDOM, api;
 
-document.addEventListener('keydown', e => {
-  game.command(event.which);
-  (event.which === 78) && publish('addPlayer', ++i)
+ws.onopen = evt => {
+  console.log(evt);
+  console.log('connected');
+  let data = {type: "join", name: 'floop'};
+  ws.send(JSON.stringify(data));
+}
+
+ws.onmessage = evt => {
+  const { type, data } = JSON.parse(evt.data);
+  console.log("WHAT GOT PARSED", type, data);
+  if(type === 'addPlayer') {
+    if(!game) {
+      gameDOM = new GameDOM(selectors, data);
+      game = new Game(data);
+      api = new Api(ws);
+      return;
+    }
+    publish('addPlayer', data);
+  }
+
+  if(type === 'removePlayer') {
+    publish('removePlayer', data);
+  }
+
+  if(type === 'startGame') {
+    game.start(data);
+  }
+
+  if(type === 'addPieces') {
+    console.log('DID IT HAPPEN?');
+    game.board.pieceList.addSet(data);
+  }
+
+  if(type === 'gameOver') {
+    game.gameOver(data);
+    gameDOM.gameOver(data);
+  }
+}
+
+const btn = document.getElementById('test');
+
+btn.addEventListener('click', () => {
+  ws.send(JSON.stringify({type: 'newGame', data: ''}))
+})
+
+document.addEventListener('keydown', event => {
+  if(game) game.command(event.which);
 });
