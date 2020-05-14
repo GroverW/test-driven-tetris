6f9cfc61fa276d6aacac395dfe4d84fda37d3801
GroverW <cheesemaker@gmail.com>
2020-05-14 11:54:37 -0700

    refactored server pubSub so unsubs don't interfere with other unsubs

--- src/helpers/pubSub.js
+++ src/helpers/pubSub.js
@@ -1,17 +1,33 @@
 const pubSub = () => ({
   subscribers: {},
   publish(topic, data) {
-    this.subscribers[topic] && this.subscribers[topic].forEach(callback => callback(data));
+    this.subscribers[topic] && this.subscribers[topic].forEach(obj => obj.callback(data));
   },
-  subscribe (topic, callback) {
-    this.subscribers[topic]
-      ? this.subscribers[topic].push(callback)
-      : this.subscribers[topic] = [callback];
-  
-    const index = this.subscribers[topic].length - 1;
-    const unsubscribe = () => { this.subscribers[topic].splice(index, 1) };
-  
+  subscribe(topic, callback) {
+    const id = this.subscribers[topic]
+      ? this.addSubscriber(topic, callback)
+      : this.addTopic(topic, callback);
+
+    const unsubscribe = () => {
+      this.removeSubscriber(topic, id);
+    };
+
     return unsubscribe;
+  },
+  addTopic(topic, callback) {
+    const id = 0;
+    this.subscribers[topic] = [{ id, callback }];
+    
+    return id;
+  },
+  addSubscriber(topic, callback) {
+    const id = this.subscribers[topic].length;
+    this.subscribers[topic].push({ id, callback });
+    
+    return id;
+  },
+  removeSubscriber(topic, id) {
+    this.subscribers[topic] = this.subscribers[topic].filter(s => s.id !== id);
   }
 })
 
