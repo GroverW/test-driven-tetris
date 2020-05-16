3924ce8e36652cd05cd9432d80b6202a9b7fbdd6
GroverW <cheesemaker@gmail.com>
2020-05-16 11:34:12 -0700

    updated main.js to get gameId before opening WS - sends get request to get gameId, then creates WS - adds handling of ws.close from server - blurs start button after it's clicked

--- frontend/static/js/main.js
+++ frontend/static/js/main.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 const Game = require('./game');
 const GameDOM = require('./gameDOM');
 const { publish } = require('../../helpers/pubSub');
@@ -15,64 +17,82 @@ const selectors = {
   playerSelector: document.getElementById('p1'),
 }
 
-const gameId = 1;
-const ws = new WebSocket(`ws://localhost:3000/game/${gameId}`);
+let gameId;
 
-let game, gameDOM, api;
+(async () => {
+  try {
+    const response = await axios.get('/game');
 
-ws.onopen = evt => {
-  console.log(evt);
-  console.log('connected');
-  let data = {type: "join", name: 'floop'};
-  ws.send(JSON.stringify(data));
-}
+    gameId = response.data.gameId;
+  } catch (err) {
+    console.log(err.response);
+  }
 
-ws.onmessage = evt => {
-  const { type, data } = JSON.parse(evt.data);
-  console.log("WHAT GOT PARSED", type, data);
-  if(type === 'addPlayer') {
-    if(!game) {
-      gameDOM = new GameDOM(selectors, data);
-      game = new Game(data);
-      api = new Api(ws);
-      return;
+
+  const ws = new WebSocket(`ws://localhost:3000/game/${gameId}`);
+
+  let game, gameDOM, api;
+
+  ws.onopen = evt => {
+    console.log(evt);
+    console.log('connected');
+    let data = { type: "join", name: 'floop' };
+    ws.send(JSON.stringify(data));
+  }
+
+  ws.onclose = evt => {
+    console.log('connection closed by server');
+    console.log(evt);
+  }
+
+  ws.onmessage = evt => {
+    const { type, data } = JSON.parse(evt.data);
+    console.log("WHAT GOT PARSED", type, data);
+    if (type === 'addPlayer') {
+      if (!game) {
+        gameDOM = new GameDOM(selectors, data);
+        game = new Game(data);
+        api = new Api(ws);
+        return;
+      }
+      publish('addPlayer', data);
     }
-    publish('addPlayer', data);
+
+    if (type === 'removePlayer') {
+      publish('removePlayer', data);
+    }
+
+    if (type === 'startGame') {
+      game.start(data);
+    }
+
+    if (type === 'updatePlayer') {
+      publish('updatePlayerBoard', data);
+    }
+
+    if (type === 'addPieces') {
+      console.log('DID IT HAPPEN?');
+      game.board.pieceList.addSet(data);
+    }
+
+    if (type === 'gameOver') {
+      game.gameOver(data);
+      gameDOM.gameOver(data);
+    }
   }
 
-  if(type === 'removePlayer') {
-    publish('removePlayer', data);
-  }
+  const btn = document.getElementById('test');
 
-  if(type === 'startGame') {
-    game.start(data);
-  }
+  btn.addEventListener('click', event => {
+    event.target.blur();
+    ws.send(JSON.stringify({ type: 'newGame', data: '' }))
+  })
 
-  if(type === 'updatePlayer') {
-    publish('updatePlayerBoard', data);
-  }
+  document.addEventListener('keydown', event => {
+    if (game) game.toggleMove(event.which, 'down');
+  });
 
-  if(type === 'addPieces') {
-    console.log('DID IT HAPPEN?');
-    game.board.pieceList.addSet(data);
-  }
-
-  if(type === 'gameOver') {
-    game.gameOver(data);
-    gameDOM.gameOver(data);
-  }
-}
-
-const btn = document.getElementById('test');
-
-btn.addEventListener('click', () => {
-  ws.send(JSON.stringify({type: 'newGame', data: ''}))
-})
-
-document.addEventListener('keydown', event => {
-  if(game) game.toggleMove(event.which, 'down');
-});
-
-document.addEventListener('keyup', event => {
-  if(game) game.toggleMove(event.which, 'up');
-})
+  document.addEventListener('keyup', event => {
+    if (game) game.toggleMove(event.which, 'up');
+  })
+})();
