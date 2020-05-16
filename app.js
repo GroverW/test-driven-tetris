7203e869a2349b167e3fb66adee25ccc4d00b05c
GroverW <cheesemaker@gmail.com>
2020-05-16 11:32:07 -0700

    added route to get new game, and check for game to app

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const GameServer = require('./src/js/gameServer');
 const Player = require('./src/js/player');
 const pubSub = require('./src/helpers/pubSub');
+const { v4: uuid } = require('uuid');
 // const server = require('http').Server(app)
 // const io = require('socket.io')(server);
 const wsExpress = require('express-ws')(app);
@@ -12,15 +13,30 @@ const wsExpress = require('express-ws')(app);
 // serve stuff in static/ folder
 app.use(express.static('frontend/static/'));
 
+
+// Get gameId and create game
+app.get('/game', (req, res, next) => {
+  const newGameId = uuid();
+
+  const gameId = GameServer.addGame(newGameId);
+
+  return res.json({ gameId });
+})
+
 /**Handle websocket messages */
 
 //allow for app.ws routes for websocket routes
 app.ws('/game/:gameId', (ws, req, next) => {
-  try {
+  try {    
     let gameServer;
     let player;
 
-    gameServer = GameServer.get(req.params.gameId)
+    gameServer = GameServer.getGame(req.params.gameId)
+
+    if(!gameServer) {
+      ws.close(1008, 'Game not found');
+      throw new Error('Invalid GameId');
+    }
 
     player = new Player(
       ws.send.bind(ws),
@@ -42,7 +58,7 @@ app.ws('/game/:gameId', (ws, req, next) => {
   catch (err) {
     console.error(err);
   }
-})
+});
 
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/frontend/static/index.html`);
