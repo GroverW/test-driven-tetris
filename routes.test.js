602e94e9975ac108de182cbfa5cc93bed249e80b
GroverW <cheesemaker@gmail.com>
2020-05-24 18:18:12 -0700

    added tests for getting games by game type

--- routes.test.js
+++ routes.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 const GameServer = require('./src/js/gameServer');
-const { GAMES } = require('./src/helpers/data');
+const { GAMES, GAME_TYPES } = require('./src/helpers/data');
 
 const app = require('./app');
 
@@ -11,23 +11,39 @@ describe('Routes tests', () => {
 
   describe('GET game', () => {
     test('Returns uuid', async () => {
-      const response = await request(app).get('/game');
+      const response = await request(app).get('/game/multi');
 
       expect(response.statusCode).toBe(200);
 
       expect(response.body.gameId).toEqual(expect.any(String));
     });
 
-    test('Creates new game with uuid', async () => {
+    test('Creates new game with uuid - multiplayer', async () => {
       expect(GAMES.size).toBe(0);
 
-      const response = await request(app).get('/game');
+      const response = await request(app).get('/game/multi');
 
       expect(response.statusCode).toBe(200);
 
       expect(response.body.gameId).toEqual(expect.any(String));
 
       expect(GAMES.size).toBe(1);
+      expect(GAMES.get(response.body.gameId).gameType).toBe(GAME_TYPES.MULTI);
+
+      expect(GAMES.get(response.body.gameId)).toEqual(expect.any(GameServer));
+    });
+    
+    test('Creates new game with uuid - single player', async () => {
+      expect(GAMES.size).toBe(0);
+
+      const response = await request(app).get('/game/single');
+
+      expect(response.statusCode).toBe(200);
+
+      expect(response.body.gameId).toEqual(expect.any(String));
+
+      expect(GAMES.size).toBe(1);
+      expect(GAMES.get(response.body.gameId).gameType).toBe(GAME_TYPES.SINGLE);
 
       expect(GAMES.get(response.body.gameId)).toEqual(expect.any(GameServer));
     });
