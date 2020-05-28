9d0e9259ddc482dfce274d6dbd5c4e604434fb63
GroverW <cheesemaker@gmail.com>
2020-05-27 18:41:21 -0700

    updated some imports in clientBoard tests

--- frontend/__tests__/clientBoard.test.js
+++ frontend/__tests__/clientBoard.test.js
@@ -1,33 +1,31 @@
 const ClientBoard = require('../static/js/clientBoard');
 const { PIECE_TYPES, ROTATE_LEFT } = require('../helpers/clientConstants');
-const { getTestBoard, getTestPieces } = require('../../common/__tests__/helpers/mocks');
+const { getTestBoard, getTestPieces } = require('../../common/mockData/mocks');
 const { Piece } = require('../../common/js/piece');
-const { publish } = require('../helpers/pubSub');
+const pubSub = require('../helpers/pubSub');
 const { pubSubMocks, TEST_BOARDS } = require('../helpers/mocks');
 
 describe('client - game board tests', () => {
   let gameBoard;
   let p1;
-  let publishTest;
-  let pubSub;
+  let pubSubListener;
 
   beforeEach(() => {
-    publishTest = publish;
-    gameBoard = new ClientBoard(publishTest);
+    gameBoard = new ClientBoard(pubSub);
     gameBoard.pieceList.pieces.push(getTestPieces());
     p1 = new Piece(PIECE_TYPES.I);
-    pubSub = pubSubMocks();
+    pubSubListener = pubSubMocks();
   })
 
   test('publishes on points from movement', () => {
     gameBoard.piece = p1;
     gameBoard.movePiece(1,0);
 
-    expect(pubSub.drawMock).toHaveBeenCalledTimes(1);
+    expect(pubSubListener.drawMock).toHaveBeenCalledTimes(1);
 
     gameBoard.movePiece(0,1);
     
-    expect(pubSub.drawMock).toHaveBeenCalledTimes(2);
+    expect(pubSubListener.drawMock).toHaveBeenCalledTimes(2);
   });
 
   test('publish board updates on line clear', () => {
@@ -41,7 +39,7 @@ describe('client - game board tests', () => {
 
     // 1 for adding piece to board
     // 1 for clearing lines
-    expect(pubSub.clearMock).toHaveBeenCalledTimes(1);
-    expect(pubSub.boardMock).toHaveBeenCalledTimes(1);
+    expect(pubSubListener.clearMock).toHaveBeenCalledTimes(1);
+    expect(pubSubListener.boardMock).toHaveBeenCalledTimes(1);
   });
 });
