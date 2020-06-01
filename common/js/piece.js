2d8eedd3b12e9ca538051453dbc6073ea3e6ef5e
GroverW <cheesemaker@gmail.com>
2020-06-01 14:40:24 -0700

    added jsdocs to common piece class

--- common/js/piece.js
+++ common/js/piece.js
@@ -1,16 +1,30 @@
 const { PIECES, BOARD_WIDTH } = require('common/helpers/constants');
 
+/**
+ * Represents a list of game pieces by id
+ */
 class PieceList {
+  /**
+   * @constructor
+   */
   constructor() {
     this.pieces = [];
     this.currIdx = 0;
     this.currSet = 0;
   }
 
+  /**
+   * Adds a new set of pieces to the piece list
+   * @param {array} pieces - array of piece Ids
+   */
   addSet(pieces) {
     this.pieces.push(pieces);
   }
 
+  /**
+   * Gets the next piece id
+   * @returns {number} - the id of the next piece
+   */
   getNextPiece() {
     const currentPiece = this.pieces[this.currSet][this.currIdx];
     this.currIdx++;
@@ -23,6 +37,10 @@ class PieceList {
     return currentPiece;
   }
 
+  /**
+   * Checks whether we've close to the end of the current piece list
+   * @returns {boolean} - if we're <= 15 pieces from the end of the list
+   */
   almostEmpty() {
     return (
       this.currSet === this.pieces.length - 1 && 
@@ -31,7 +49,15 @@ class PieceList {
   }
 }
 
+
+/**
+ * Represents a game piece.
+ */
 class Piece {
+  /**
+   * @constructor
+   * @param {number} piece - id of piece
+   */
   constructor(piece) {
     this.type = piece;
     this.piece = PIECES[piece];
@@ -41,11 +67,20 @@ class Piece {
     this.y = 0;
   }
 
+  /**
+   * Updates the x and y coordinatese of the piece
+   * @param {number} x - change in x-coordinate
+   * @param {number} y - change in y-coordinate
+   */
   move(x,y) {
     this.x += x;
     this.y += y;
   }
 
+  /**
+   * Updates the piece's grid based on clockwise or counter-clockwise rotation
+   * @param {number} rotation - (-1) for counter-clockwise, (1) for clockwise
+   */
   update(rotation) {
     const rotAmt = this.rotation + rotation;
     
