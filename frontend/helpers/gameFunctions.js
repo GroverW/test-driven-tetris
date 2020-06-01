d2e25db6c68418275302fc17965104cce0c2279e
GroverW <cheesemaker@gmail.com>
2020-06-01 14:41:11 -0700

    added jsdocs to gameFunctions

--- frontend/helpers/gameFunctions.js
+++ frontend/helpers/gameFunctions.js
@@ -7,11 +7,18 @@ const Api = require('./api');
 const { publish } = require('./pubSub');
 const { gameIdSelector, gameSelectors, startButton } = require('./DOMSelectors')
 
-
+/**
+ * Adds game id to stats container for sharing
+ * @param {string} id - game id
+ */
 const addGameIdToStats = (id) => {
   gameIdSelector.innerText = id;
 }
 
+/**
+ * Creates a new single, or multiplayer game
+ * @param {string} type - 'single' or 'multi'
+ */
 const createGame = async type => {
   try {
     const response = await axios.get(`/game/${type}`);
@@ -26,11 +33,18 @@ const createGame = async type => {
   }
 }
 
-const connectToGame = (gameId) => {
+/**
+ * Connects to an already created game
+ * @param {string} gameId - game id
+ */
+const connectToGame = gameId => {
   const ws = new WebSocket(`ws://localhost:3000/game/${gameId}`);
 
   let game, gameDOM, api;
 
+  /**
+   * What to do when the websocket is opened
+   */
   ws.onopen = evt => {
     console.log(evt);
     console.log('connected');
@@ -38,11 +52,17 @@ const connectToGame = (gameId) => {
     ws.send(JSON.stringify(data));
   }
 
+  /**
+   * What to do when the websocket is closed
+   */
   ws.onclose = evt => {
     console.log('connection closed by server');
     console.log(evt);
   }
 
+  /**
+   * What to do when receiving a message over the websocket
+   */
   ws.onmessage = evt => {
     const { type, data } = JSON.parse(evt.data);
     console.log("WHAT GOT PARSED", type, data);
@@ -62,7 +82,6 @@ const connectToGame = (gameId) => {
         publish('removePlayer', data);
         break;
       case 'startGame':
-        console.log('start er up')
         if(game) game.start(data);
         break;
       case 'updatePlayer':
@@ -81,6 +100,11 @@ const connectToGame = (gameId) => {
   }
 }
 
+/**
+ * Creates event listeners for starting game, and keyboard controls
+ * @param {object} game - instance of ClientGame class
+ * @param {object} api - instance of Api class
+ */
 const createEventListeners = (game, api) => {
   startButton.addEventListener('click', event => {
     event.target.blur();
