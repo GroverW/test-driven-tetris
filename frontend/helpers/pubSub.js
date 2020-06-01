22ebd008d94cc95753f206118e2d3208bddc59db
GroverW <cheesemaker@gmail.com>
2020-06-01 14:41:25 -0700

    added jsdocs to frontend pubSub

--- frontend/helpers/pubSub.js
+++ frontend/helpers/pubSub.js
@@ -1,9 +1,20 @@
 let subscribers = {};
 
+/**
+ * Publishes data to a specified topic
+ * @param {string} topic - topic to publish to
+ * @param {*} data - data to publish to topic
+ */
 const publish = (topic, data) => {
   subscribers[topic] && subscribers[topic].forEach(callback => callback(data));
 }
 
+/**
+ * Adds subscriber to a specified topic
+ * @param {string} topic - topic to subscribe to
+ * @param {function} callback - function to call when data is published to topic
+ * @returns {function} - function to call to unsubscribe from topic
+ */
 const subscribe = (topic, callback) => {
   subscribers[topic]
     ? subscribers[topic].push(callback)
