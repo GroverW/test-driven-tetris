90f95c9832db720b17a611536119b420e149d652
GroverW <cheesemaker@gmail.com>
2020-06-01 14:39:21 -0700

    added jsdocs to common board class

--- common/js/board.js
+++ common/js/board.js
@@ -2,7 +2,15 @@ const { BOARD_WIDTH, BOARD_HEIGHT, POINTS, WALL_KICK_TESTS, WALL_KICK_TESTS_I } 
 const { getEmptyBoard } = require('common/helpers/utils');
 const { PieceList, Piece } = require('./piece');
 
+/**
+ * Represents a game board
+ */
 class Board {
+  /**
+   * @constructor
+   * @param {object} pubSub - Publish / Subscribe object
+   * @param {number} playerId - Id of player linking backend to frontend
+   */
   constructor(pubSub, playerId) {
     this.playerId = playerId;
     this.grid = getEmptyBoard();
@@ -12,11 +20,15 @@ class Board {
     this.pieceList = new PieceList();
   }
 
+  /**
+   * Sets the current and next pieces
+   */
   getPieces() {
     this.piece = this.nextPiece
       ? this.nextPiece
       : new Piece(this.pieceList.getNextPiece());
     
+    // Game Over condition. If a new piece cannot be set because it's blocked
     if(!this.validMove(0,0)) {
       this.pubSub.publish("gameOver", {
         id: this.playerId,
@@ -27,7 +39,13 @@ class Board {
     this.nextPiece = new Piece(this.pieceList.getNextPiece());
   }
 
-  // server version
+  /**
+   * Moves the current piece to a new place on the board
+   * @param {number} x - The number of spaces to move in the x-direction
+   * @param {number} y - The number of spaces to move in the y-direection
+   * @param {number} multiplier [multiplier=1] - Points multiplier based on type of movement
+   * @returns {boolean} - Whether or not the move was valid.
+   */
   movePiece(x, y, multiplier = POINTS.DOWN) {
     const validMove = this.validMove(x, y);
 
@@ -42,6 +60,10 @@ class Board {
     return validMove;
   }
 
+  /**
+   * Adds the current piece to the board. Clears any completed lines,
+   * gets new pieces and publishes board updates.
+   */
   drop() {
     this.addPieceToBoard();
     this.clearLines();
@@ -49,6 +71,10 @@ class Board {
     this.publishBoardUpdate();
   }
 
+  /**
+   * Moves the current piece to the lowest, valid position possible
+   * and adds it to the board.
+   */
   hardDrop() {
     let yChange = 0;
 
@@ -58,6 +84,12 @@ class Board {
     this.drop();
   }
 
+  /**
+   * Checks whether the requested movement is valid
+   * @param {number} xChange - requested x-movement
+   * @param {number} yChange - requested y-movement
+   * @returns {boolean} - whether or not the requested move is valid
+   */
   validMove(xChange, yChange) {
     const xStart = this.piece.x + xChange;
     const yStart = this.piece.y + yChange;
@@ -71,15 +103,32 @@ class Board {
     )
   }
 
+  /**
+   * Checks whether the requested board coordinates are unoccupied
+   * @param {number} x - x-coordinate
+   * @param {number} y - y-coordinate
+   * @returns {boolean} - whether or not the requested coordinatea are unoccupied
+   */
   isEmpty(x, y) {
     return y >= 0 && this.grid[y][x] === 0;
   }
 
+  /**
+   * Checks whether the requested board coordinates are in-bounds
+   * @param {number} x - x-coordinate
+   * @param {number} y - y-coordinate
+   * @returns {boolean} - whether the requested board coordinates are in-bounds
+   */
   isInBounds(x, y) {
     return x >= 0 && x < BOARD_WIDTH && y >= 0 && y < BOARD_HEIGHT;
   }
 
+  /**
+   * Rotates the current piece clockwise or counter-clockwise
+   * @param {number} direction - (-1) for counter-clockwise, (1) for clockwise
+   */
   rotatePiece(direction) {
+    // determines which set of tests to use based on type of piece
     const lookup = this.piece.type === 1 
       ? WALL_KICK_TESTS_I
       : WALL_KICK_TESTS;
@@ -87,17 +136,23 @@ class Board {
     const tests = lookup[direction][this.piece.rotation];
     this.piece.update(direction);
 
+    // runs tests for current piece to determine if a valid rotation can be made
     for(let [xChange, yChange] of tests) {
       if(this.validMove(xChange, yChange)) {
         const diff = (xChange !== 0 || yChange !== 0);
+        // moves piece if a valid location could be found for it
         diff && this.movePiece(xChange, yChange, 0);
         return;
       }
     }
 
+    // undoes rotation if no valid position could be found
     this.piece.update(-direction);
   }
 
+  /**
+   * Adds the current piece to the board
+   */
   addPieceToBoard() {
     const xStart = this.piece.x;
     const yStart = this.piece.y;
@@ -108,6 +163,10 @@ class Board {
     });
   }
 
+  /**
+   * Clears completed lines after piece added to board
+   * @returns {number} - number of lines cleared
+   */
   clearLines() {
     let numCleared = 0;
 
@@ -124,6 +183,9 @@ class Board {
     return numCleared;
   }
 
+  /**
+   * Publishes any board updates
+   */
   publishBoardUpdate() {
     // to be implemented by server and client
   }
