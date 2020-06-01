6934fa10f6918f625135531d451329b78c191e11
GroverW <cheesemaker@gmail.com>
2020-06-01 14:40:52 -0700

    added jsdocs to clientUtils

--- frontend/helpers/clientUtils.js
+++ frontend/helpers/clientUtils.js
@@ -1,7 +1,18 @@
 const { randomize, getEmptyBoard } = require('common/helpers/utils');
 
+/**
+ * Creates an object containing new player information
+ * @param {object} ctx - player canvas context
+ * @param {array} board - player board
+ * @param {number} id - player id
+ */
 const getNewPlayer = (ctx, board, id) => ({ ctx, board, id });
 
+/**
+ * Creates an object containing a DOM selector and id of a new player
+ * @param {object} selector - DOM selector
+ * @param {*} id - player id
+ */
 const getNewPlayerDOM = (selector, id) => ({ selector, id });
 
 module.exports = {
