bc771bdff66aef9e8443c876710f1579b3582b1d
GroverW <cheesemaker@gmail.com>
2020-06-01 14:43:20 -0700

    added jsdocs to serverBoard class

--- src/js/serverBoard.js
+++ src/js/serverBoard.js
@@ -1,23 +1,41 @@
 const Board = require('common/js/board');
 
+
+/**
+ * Represents a server-side game board
+ */
 class ServerBoard extends Board {
+  /**
+ * @constructor
+ * @param {object} pubSub - Publish / Subscribe object
+ * @param {number} playerId - Id of player on backend
+ */
   constructor(pubSub, playerId) {
     super(pubSub, playerId);
   }
 
+  /**
+   * Sets the current and next pieces
+   */
   getPieces() {
     super.getPieces();
-    
-    if(this.pieceList.almostEmpty()) {
+
+    if (this.pieceList.almostEmpty()) {
       this.pubSub.publish('getPieces');
     }
   }
 
+  /**
+   * Clears completed lines after piece added to board
+   */
   clearLines() {
     const numCleared = super.clearLines();
     numCleared && this.publishBoardUpdate();
   }
-
+  
+  /**
+   * Publishes any board updates
+   */
   publishBoardUpdate() {
     this.pubSub.publish('updateBoard', {
       id: this.id,
