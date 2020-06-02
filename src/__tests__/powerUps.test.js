96f75c7f680725f6d4611d46fffa37cd46f459e9
GroverW <cheesemaker@gmail.com>
2020-06-01 21:03:34 -0700

    added swap, scramble and clear power up tests

--- src/__tests__/powerUps.test.js
+++ src/__tests__/powerUps.test.js
@@ -1,12 +1,15 @@
 const { getTestBoard } = require('common/mockData/mocks');
 const {
-  swapLines
+  swapLines,
+  swapBoards,
+  scrambleBoard,
+  clearBoard,
 } = require('backend/helpers/powerUps');
 
 describe('power ups', () => {
-  beforeEach(() => {
-
-  })
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
   
   test('swap lines', () => {
     const board1 = getTestBoard('pattern1');
@@ -26,14 +29,30 @@ describe('power ups', () => {
   });
 
   test('swap board', () => {
+    const board1 = getTestBoard('pattern1');
+    const board2 = getTestBoard('pattern2');
 
+    const [result1, result2] = swapBoards(board1, board2);
+
+    expect(result1).toEqual(board2);
+    expect(result2).toEqual(board1);
   });
 
   test('scramble board', () => {
+    Math.random = jest.fn().mockReturnValue(.5)
 
+    const board = getTestBoard('pattern2');
+
+    const result = scrambleBoard(board);
+
+    expect(result).toEqual(getTestBoard('pattern2Scrambled'));
   });
 
   test('clear board', () => {
+    const board = getTestBoard('pattern5');
 
+    const result = clearBoard(board);
+
+    expect(result).toEqual(getTestBoard('empty'));
   });
 });
