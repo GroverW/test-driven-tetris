24ead64fdb55a724718d2da007cfbf857d8b680e
GroverW <cheesemaker@gmail.com>
2020-06-02 16:43:42 -0700

    moved some power up constants to common from server

--- src/helpers/serverConstants.js
+++ src/helpers/serverConstants.js
@@ -13,25 +13,10 @@ const CONTROLS = new Set([
   ...COMMON_CONSTANTS.PLAYERS,
 ]);
 
-const POWER_UP_TYPES = {
-  SWAP_LINES: 1,
-  SWAP_BOARD: 2,
-  SCRAMBLE_BOARD: 3,
-  CLEAR_BOARD: 4,
-};
-const POWER_UPS = new Set([
-  POWER_UP_TYPES.SWAP_LINES,
-  POWER_UP_TYPES.SWAP_BOARD,
-  POWER_UP_TYPES.SCRAMBLE_BOARD,
-  POWER_UP_TYPES.CLEAR_BOARD,
-])
-
 const POWER_UP_LIST = [
   0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
   1,1,1,1,1,1,1,1,1,2,2,2,3,3,3,4,4,4,
 ]
-
-const MAX_POWER_UPS = 2;
 
 const RANKINGS = {
   1: "1st",
@@ -44,9 +29,6 @@ module.exports = {
   ...COMMON_CONSTANTS,
   GAMES,
   CONTROLS,
-  POWER_UP_TYPES,
-  POWER_UPS,
   POWER_UP_LIST,
-  MAX_POWER_UPS,
   RANKINGS,
 };
