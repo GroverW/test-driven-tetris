bdf06e9c1dc716b902bda07ebf820d980cc9da62
GroverW <cheesemaker@gmail.com>
2020-06-02 21:25:17 -0700

    added player test for setting game type

--- src/__tests__/player.test.js
+++ src/__tests__/player.test.js
@@ -2,6 +2,7 @@ const Player = require('backend/js/player');
 const ServerGame = require('backend/js/serverGame');
 const pubSub = require('backend/helpers/pubSub');
 const { mockSend } = require('common/mockData/mocks')
+const { GAME_TYPES } = require('backend/helpers/serverConstants');
 
 
 describe('player tests', () => {
@@ -20,5 +21,21 @@ describe('player tests', () => {
   test('setup player', () => {
     expect(p1.game).toEqual(expect.any(ServerGame));
     expect(p2.game).toEqual(expect.any(ServerGame));
+  });
+
+  test('set id', () => {
+    expect(p1.id).toBe(undefined);
+    
+    p1.setId(1);
+
+    expect(p1.id).toBe(1);
+    expect(p1.game.playerId).toBe(1);
+    expect(p1.game.board.playerId).toBe(1);
+  })
+
+  test('set game type', () => {
+    expect(p1.game.gameType).toBe(undefined);
+    p1.setGameType(GAME_TYPES.MULTI);
+    expect(p1.game.gameType).toBe(GAME_TYPES.MULTI);
   });
 });
