3546b6b5d5a7af4aff369f10445b1712890f8147
GroverW <cheesemaker@gmail.com>
2020-06-04 11:16:40 -0700

    added jsdoc strings to common mocks

--- common/mockData/mocks.js
+++ common/mockData/mocks.js
@@ -2,13 +2,28 @@ const TEST_BOARDS = require('./sampleBoards');
 const { SEED_PIECES } = require('common/helpers/constants');
 const { randomize } = require('common/helpers/utils');
 
+/**
+ * Used for adding fake websocket .send
+ */
 const mockSend = () => {};
 
+/**
+ * Creates new test board from sampleBoards
+ * @param {string} board - name of test board
+ * @returns {array} - test board
+ */
 const getTestBoard = (board) =>
   JSON.parse(JSON.stringify(TEST_BOARDS[board]));
 
+  /**
+   * Creates list of piece ids
+   * @returns {array} - new array of piece ids
+   */
 const getTestPieces = () => randomize(SEED_PIECES);
 
+/**
+ * Simple pubSub object to mock websocket functionality
+ */
 const webSocketMock = {
   topics: {},
   send(data) {
