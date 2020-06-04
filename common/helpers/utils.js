704ac9c438e534f3d180682f4f3e5b81bf98d5e3
GroverW <cheesemaker@gmail.com>
2020-06-04 11:15:59 -0700

    added mapArrayToObject to common utils

--- common/helpers/utils.js
+++ common/helpers/utils.js
@@ -1,4 +1,10 @@
 const { BOARD_WIDTH, BOARD_HEIGHT } = require('./constants');
+
+/**
+ * Randomizes elements in an array
+ * @param {array} arr 
+ * @returns {array} - new copy of array with elements randomized
+ */
 const randomize = arr => {
   let start = arr.length - 1;
   let randomized = [...arr];
@@ -12,9 +18,26 @@ const randomize = arr => {
   return randomized;
 }
 
+/**
+ * Returns a new, empty board
+ */
 const getEmptyBoard = () => new Array(BOARD_HEIGHT).fill(null).map(() => Array(BOARD_WIDTH).fill(0));
+
+/**
+ * Maps keys in an array to values in an object. O(n) vs O(n^2) for Array.reduce
+ * @param {array} keysArr - list of keys to add to object
+ * @param {function} callback - callback to create object values
+ * @param {object} resObj - object to add elements to
+ * @returns {object} - object with keys mapped to values
+ */
+const mapArrayToObj = (keysArr, callback, resObj = {}) => {
+  keysArr.forEach((key, idx, keysArr) => { resObj[key] = callback(key, idx, keysArr) });
+
+  return resObj;
+}
 
 module.exports = {
   randomize,
   getEmptyBoard,
+  mapArrayToObj,
 }
