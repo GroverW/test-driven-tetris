98cbd2b2748baefeb58612b5c2df5ebf581c8339
GroverW <cheesemaker@gmail.com>
2020-06-19 12:33:41 -0700

    moved some clientTopics to commonTopics

--- common/helpers/commonTopics.js
+++ common/helpers/commonTopics.js
@@ -2,6 +2,8 @@
 /**
  * Common publish / subscribe topics
  */
+const ADD_ERROR = 'addError';
+const PLAY = 'play';
 const START_GAME = 'startGame';
 const GAME_OVER = 'gameOver';
 const LOWER_PIECE = 'lowerPiece';
@@ -9,8 +11,14 @@ const CLEAR_LINES = 'clearLines';
 const ADD_PLAYER = 'addPlayer';
 const REMOVE_PLAYER = 'removePlayer';
 const UPDATE_PLAYER = 'updatePlayer';
+const ADD_PIECES = 'addPieces';
+const EXECUTE_COMMANDS = 'executeCommands';
+const ADD_POWER_UP = 'addPowerUp';
+const USE_POWER_UP = 'usePowerUp';
 
 module.exports = {
+  ADD_ERROR,
+  PLAY,
   START_GAME,
   GAME_OVER,
   LOWER_PIECE,
@@ -18,4 +26,8 @@ module.exports = {
   ADD_PLAYER,
   REMOVE_PLAYER,
   UPDATE_PLAYER,
+  ADD_PIECES,
+  EXECUTE_COMMANDS,
+  ADD_POWER_UP,
+  USE_POWER_UP,
 };
