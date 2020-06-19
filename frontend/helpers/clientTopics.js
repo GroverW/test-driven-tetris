b80bf66b718c01bd61e60ff42a9e04f5af2143d2
GroverW <cheesemaker@gmail.com>
2020-06-19 12:36:34 -0700

    moved some clientTopics to commonTopics

--- frontend/helpers/clientTopics.js
+++ frontend/helpers/clientTopics.js
@@ -1,12 +1,12 @@
 const COMMON_TOPICS = require('common/helpers/commonTopics');
 
+/**
+ * Client publish / subscribe topics
+ */
 const DRAW = 'draw';
 const BOARD_CHANGE = 'boardChange';
-const ADD_ERROR = 'addError';
 const CLEAR_ERROR = 'clearError';
 const TOGGLE_MENU = 'toggleMenu';
-const ADD_POWER_UP = 'addPowerUp';
-const USE_POWER_UP = 'usePowerUp';
 const UPDATE_SCORE = 'updateScore';
 const SEND_MESSAGE = 'sendMessage';
 
@@ -14,11 +14,8 @@ module.exports = {
   ...COMMON_TOPICS,
   DRAW,
   BOARD_CHANGE,
-  ADD_ERROR,
   CLEAR_ERROR,
   TOGGLE_MENU,
-  ADD_POWER_UP,
-  USE_POWER_UP,
   UPDATE_SCORE,
   SEND_MESSAGE,
 };
