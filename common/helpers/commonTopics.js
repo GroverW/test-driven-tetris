5f717b70c7b61db0e4abfcf6acb70ceb1cebb42d
GroverW <cheesemaker@gmail.com>
2020-06-19 11:02:27 -0700

    added commonTopics

---
+++ common/helpers/commonTopics.js
@@ -0,0 +1,21 @@
+
+/**
+ * Common publish / subscribe topics
+ */
+const START_GAME = 'startGame';
+const GAME_OVER = 'gameOver';
+const LOWER_PIECE = 'lowerPiece';
+const CLEAR_LINES = 'clearLines';
+const ADD_PLAYER = 'addPlayer';
+const REMOVE_PLAYER = 'removePlayer';
+const UPDATE_PLAYER = 'updatePlayer';
+
+module.exports = {
+  START_GAME,
+  GAME_OVER,
+  LOWER_PIECE,
+  CLEAR_LINES,
+  ADD_PLAYER,
+  REMOVE_PLAYER,
+  UPDATE_PLAYER,
+};
