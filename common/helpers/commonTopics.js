3bbf255a5cbaeb6e973e4d98a1cfb3efab0301d9
GroverW <cheesemaker@gmail.com>
2020-06-22 15:19:12 -0700

    reorganized commonTopics

--- common/helpers/commonTopics.js
+++ common/helpers/commonTopics.js
@@ -3,16 +3,21 @@
  * Common publish / subscribe topics
  */
 const ADD_ERROR = 'addError';
+
 const PLAY = 'play';
 const START_GAME = 'startGame';
 const GAME_OVER = 'gameOver';
+
+const ADD_PIECES = 'addPieces';
 const LOWER_PIECE = 'lowerPiece';
 const CLEAR_LINES = 'clearLines';
+
 const ADD_PLAYER = 'addPlayer';
 const REMOVE_PLAYER = 'removePlayer';
 const UPDATE_PLAYER = 'updatePlayer';
-const ADD_PIECES = 'addPieces';
+
 const EXECUTE_COMMANDS = 'executeCommands';
+
 const ADD_POWER_UP = 'addPowerUp';
 const USE_POWER_UP = 'usePowerUp';
 
@@ -21,12 +26,12 @@ module.exports = {
   PLAY,
   START_GAME,
   GAME_OVER,
+  ADD_PIECES,
   LOWER_PIECE,
   CLEAR_LINES,
   ADD_PLAYER,
   REMOVE_PLAYER,
   UPDATE_PLAYER,
-  ADD_PIECES,
   EXECUTE_COMMANDS,
   ADD_POWER_UP,
   USE_POWER_UP,
