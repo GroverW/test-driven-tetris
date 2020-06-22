5008ac86ab0dacef728f5dd1ec9a7e652d6beef6
GroverW <cheesemaker@gmail.com>
2020-06-22 15:21:43 -0700

    reorganized clientTopics

--- frontend/helpers/clientTopics.js
+++ frontend/helpers/clientTopics.js
@@ -3,19 +3,22 @@ const COMMON_TOPICS = require('common/helpers/commonTopics');
 /**
  * Client publish / subscribe topics
  */
+const CLEAR_ERROR = 'clearError';
+
+const TOGGLE_MENU = 'toggleMenu';
+
 const DRAW = 'draw';
 const BOARD_CHANGE = 'boardChange';
-const CLEAR_ERROR = 'clearError';
-const TOGGLE_MENU = 'toggleMenu';
 const UPDATE_SCORE = 'updateScore';
+
 const SEND_MESSAGE = 'sendMessage';
 
 module.exports = {
   ...COMMON_TOPICS,
-  DRAW,
-  BOARD_CHANGE,
   CLEAR_ERROR,
   TOGGLE_MENU,
+  DRAW,
+  BOARD_CHANGE,
   UPDATE_SCORE,
   SEND_MESSAGE,
 };
