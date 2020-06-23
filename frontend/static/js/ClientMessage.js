61fe36ce36d8d7b508b132a66a6b322ad5e7a01e
GroverW <cheesemaker@gmail.com>
2020-06-23 14:54:21 -0700

    changed ClientError class to ClientMessage

---
+++ frontend/static/js/ClientMessage.js
@@ -0,0 +1,36 @@
+const { subscribe } = require('frontend/helpers/pubSub');
+const { MESSAGE_TIMEOUT } = require('frontend/helpers/clientConstants');
+const { ADD_MESSAGE, CLEAR_MESSAGE } = require('frontend/helpers/clientTopics');
+
+class ClientMessage {
+  constructor(messageSelector) {
+    this.message = messageSelector;
+    this.subscriptions = [
+      subscribe(ADD_MESSAGE, this.handleMessage.bind(this)),
+      subscribe(CLEAR_MESSAGE, this.clearMessage.bind(this)),
+    ];
+  }
+
+  addMessage(type, message) {
+    this.message.innerText = message;
+    
+    this.message.classList = '';
+    this.message.classList.add(type);
+  }
+
+  clearMessage() {
+    this.message.classList.add('hide');
+  }
+
+  handleMessage({ type, message }) {
+    this.addMessage(type, message);
+
+    setTimeout(this.clearMessage.bind(this), MESSAGE_TIMEOUT);
+  }
+
+  unsubscribe() {
+    this.subscriptions.forEach(unsub => unsub());
+  }
+}
+
+module.exports = ClientMessage;
