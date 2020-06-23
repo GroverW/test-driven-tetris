233eed6fe0d8b02444502f324a6d9ca9a35d5ae2
GroverW <cheesemaker@gmail.com>
2020-06-23 14:54:05 -0700

    changed ClientError tests to ClientMessage tests

---
+++ frontend/__tests__/ClientMessage.test.js
@@ -0,0 +1,77 @@
+const ClientMessage = require('frontend/static/js/ClientMessage');
+const { publish } = require('frontend/helpers/pubSub');
+const { getMockDOMSelector } = require('frontend/mockData/mocks');
+const { MESSAGE_TIMEOUT } = require('frontend/helpers/clientConstants');
+const { ADD_MESSAGE, CLEAR_MESSAGE, MSG_TYPE } = require('frontend/helpers/clientTopics');
+
+describe('client message tests', () => {
+  let clientMessage;
+  const messageText = 'some message';
+  const errorData = {
+    type: MSG_TYPE.ERROR,
+    message: messageText,
+  };
+  const noticeData = {
+    type: MSG_TYPE.NOTICE,
+    message: messageText,
+  }
+
+  beforeEach(() => {
+    const messageSelector = getMockDOMSelector();
+    messageSelector.classList.add('hide');
+    clientMessage = new ClientMessage(messageSelector);
+
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    clientMessage.unsubscribe();
+  });
+
+  test('add error message', () => {
+    expect(clientMessage.message.classList.contains('hide')).toBe(true);
+
+    publish(ADD_MESSAGE, errorData);
+
+    expect(clientMessage.message.innerText).toBe(messageText);
+    expect(clientMessage.message.classList.contains(MSG_TYPE.ERROR)).toBe(true);
+    expect(clientMessage.message.classList.contains('hide')).toBe(false);
+  });
+
+  test('add notice message', () => {
+    expect(clientMessage.message.classList.contains('hide')).toBe(true);
+
+    publish(ADD_MESSAGE, noticeData);
+
+    expect(clientMessage.message.innerText).toBe(messageText);
+    expect(clientMessage.message.classList.contains(MSG_TYPE.NOTICE)).toBe(true);
+    expect(clientMessage.message.classList.contains('hide')).toBe(false);
+  });
+
+  test('clear error message', () => {
+    expect(clientMessage.message.classList.contains('hide')).toBe(true);
+
+    publish(ADD_MESSAGE, errorData);
+
+    expect(clientMessage.message.innerText).toBe(messageText);
+    expect(clientMessage.message.classList.contains('hide')).toBe(false);
+
+    publish(CLEAR_MESSAGE);
+
+    expect(clientMessage.message.classList.contains('hide')).toBe(true);
+  });
+
+  test('handle error', () => {
+    expect(clientMessage.message.classList.contains('hide')).toBe(true);
+
+    publish(ADD_MESSAGE, errorData);
+
+    expect(clientMessage.message.innerText).toBe(messageText);
+    expect(clientMessage.message.classList.contains('hide')).toBe(false);
+
+    jest.advanceTimersByTime(MESSAGE_TIMEOUT);
+;
+    expect(clientMessage.message.classList.contains('hide')).toBe(true);
+  });
+});
