144e59a41a9f66a42974972ee38e4cab3d46b2d7
GroverW <cheesemaker@gmail.com>
2020-06-25 15:53:40 -0700

    added DOMUtils for editing/creating html elements

---
+++ frontend/helpers/DOMUtils.js
@@ -0,0 +1,35 @@
+const addText = (selector, text) => {
+  selector.innerText = text;
+}
+
+/**
+ * Creates and returns a new HTML Element
+ * @param {string} type - element tag
+ * @param {object} [container] - a container this element should be appended to
+ * @param {string} [id] - id to give element 
+ * @param {string} [class] - class to give element 
+ * @param {string} [text] - element text to display
+ */
+const createElement = (type, { container, id, classList, text }) => {
+  let newElement = document.createElement(type);
+  
+  if (id) newElement.id = id;
+  if (classList) newElement.classList.add(classList);
+  if (text) addText(newElement, text);
+  if (container) container.appendChild(newElement);
+
+  return newElement;
+}
+
+const addPowerUpTargetId = (container, id) => 
+  createElement('div', {
+    container,
+    classList: 'power-up-target',
+    text: id,
+  });
+
+module.exports = {
+  addText,
+  createElement,
+  addPowerUpTargetId,
+}
