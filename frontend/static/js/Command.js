e3c2bfb27c91b34e552e273b440d1265176df81b
GroverW <cheesemaker@gmail.com>
2020-07-03 11:34:44 -0700

    added Command class

---
+++ frontend/static/js/Command.js
@@ -0,0 +1,36 @@
+class Command {
+  constructor(key, callback, delay=0, toggle=false) {
+    this.key = key;
+    this.callback = callback;
+    this.toggle = toggle;
+    this.toggleIdx = 0;
+    this.startTime;
+    this.delay = this.getInitialDelay(delay);
+  }
+
+  getInitialDelay(delay) {
+    return this.toggle ? this.toggle[this.toggleIdx] : delay;
+  }
+
+  execute(time) {
+    if(this.startTime === undefined) this.startTime = time;
+
+    if(time >= this.startTime + this.delay) {
+      this.callback();
+
+      if(this.toggle) {
+        this.handleToggle();
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  handleToggle() {
+    this.toggleIdx = Math.min(this.toggle.length - 1, this.toggleIdx + 1);
+    this.delay = this.toggle[this.toggleIdx];
+  }
+}
+
+module.exports = Command;
