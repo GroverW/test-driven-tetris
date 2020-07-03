69d1de9d1909c1c29ef378b93b55c8f6d4e85ad2
GroverW <cheesemaker@gmail.com>
2020-07-03 13:53:17 -0700

    added checks in setCommand and clearCommand in GameLoop class

--- frontend/static/js/GameLoop.js
+++ frontend/static/js/GameLoop.js
@@ -6,16 +6,24 @@ class GameLoop {
   }
 
   setCommand(command) {
-    this.command = command;
+    if (this.command === undefined || this.command.key !== command.key) {
+      this.command = command;
+    }
   }
 
   setAutoCommand(command) {
     this.autoCommand = command;
   }
 
+  clearCommand(key) {
+    if (this.command !== undefined && key === this.command.key) {
+      this.command = undefined;
+    }
+  }
+
   animate(currTime = 0) {
-    if(this.command) this.command(currTime);
-    if(this.autoCommand) this.autoCommand(currTime);
+    if (this.command !== undefined) this.command.execute(currTime);
+    if (this.autoCommand !== undefined) this.autoCommand.execute(currTime);
 
     this.animationId = requestAnimationFrame(this.animate.bind(this))
   }
