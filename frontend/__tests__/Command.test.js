602aa56bef2e5b9dcabe0d73aef65a6c60413b73
GroverW <cheesemaker@gmail.com>
2020-07-03 12:42:58 -0700

    reorganized Command tests

--- frontend/__tests__/Command.test.js
+++ frontend/__tests__/Command.test.js
@@ -15,63 +15,71 @@ describe('Command Tests', () => {
     jest.clearAllMocks();
   });
 
-  test('get initial delay', () => {
-    const delay = command.getInitialDelay(100);
-    expect(delay).toBe(100);
-
-    expect(command.delay).toBe(1);
-    expect(commandToggle.delay).toBe(10);
+  describe('setup', () => {
+    test('get initial delay', () => {
+      const delay = command.getInitialDelay(100);
+      expect(delay).toBe(100);
+  
+      expect(command.delay).toBe(1);
+      expect(commandToggle.delay).toBe(10);
+    });
   });
 
-  test('execute - sets start time on first call', () => {
-    expect(command.startTime).toBe(undefined);
-    
-    command.execute(0);
-    
-    expect(command.startTime).toBe(0);
+  describe('execute command', () => {
+    test('sets start time on first call', () => {
+      expect(command.startTime).toBe(undefined);
+      
+      command.execute(0);
+      
+      expect(command.startTime).toBe(0);
+    });
+  
+    test('delay time met', () => {
+      expect(callback).toHaveBeenCalledTimes(0);
+      
+      command.execute(1);
+  
+      expect(callback).toHaveBeenCalledTimes(1);
+    });
+  
+    test('delay time not met', () => {
+      expect(callbackToggle).toHaveBeenCalledTimes(0);
+  
+      commandToggle.execute(0);
+  
+      expect(callbackToggle).toHaveBeenCalledTimes(0);
+    });
   });
 
-  test('execute - delay time met', () => {
-    expect(callback).toHaveBeenCalledTimes(0);
-    
-    command.execute(1);
+  describe('toggle command', () => {
+    test('handles toggling', () => {
+      expect(commandToggle.delay).toBe(10);
+  
+      commandToggle.handleToggle();
+  
+      expect(commandToggle.delay).toBe(100);
+    });
+  
+    test('handles toggling on successful execute', () => {
+      const handleToggleSpy = jest.spyOn(commandToggle, 'handleToggle');
+  
+      commandToggle.execute(50);
+      
+      expect(handleToggleSpy).toHaveBeenCalledTimes(0);
+      
+      commandToggle.execute(150);
+  
+      expect(handleToggleSpy).toHaveBeenCalledTimes(1);
+  
+      expect(commandToggle.delay).toBe(200);
+    });
+  
+    test('does not go over max delay', () => {
+      expect(commandToggle.toggleIdx).toBe(2);
 
-    expect(callback).toHaveBeenCalledTimes(1);
-  });
-
-  test('execute - delay time not met', () => {
-    expect(callbackToggle).toHaveBeenCalledTimes(0);
-
-    commandToggle.execute(0);
-
-    expect(callbackToggle).toHaveBeenCalledTimes(0);
-  });
-
-  test('handle toggle', () => {
-    expect(commandToggle.delay).toBe(10);
-
-    commandToggle.handleToggle();
-
-    expect(commandToggle.delay).toBe(100);
-  });
-
-  test('handle toggle - called on successful execute', () => {
-    const handleToggleSpy = jest.spyOn(commandToggle, 'handleToggle');
-
-    commandToggle.execute(50);
-    
-    expect(handleToggleSpy).toHaveBeenCalledTimes(0);
-    
-    commandToggle.execute(150);
-
-    expect(handleToggleSpy).toHaveBeenCalledTimes(1);
-
-    expect(commandToggle.delay).toBe(200);
-  });
-
-  test('handle toggle - does not go over max delay', () => {
-    expect(commandToggle.toggleIdx).toBe(2);
-    commandToggle.handleToggle();
-    expect(commandToggle.toggleIdx).toBe(2);
+      commandToggle.handleToggle();
+      
+      expect(commandToggle.toggleIdx).toBe(2);
+    });
   });
 });
