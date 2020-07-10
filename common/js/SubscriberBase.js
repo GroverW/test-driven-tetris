e3440f6feeecec378af08a8753089705a95e2600
GroverW <cheesemaker@gmail.com>
2020-07-10 15:20:57 -0700

    added methods for game over and end game

--- common/js/SubscriberBase.js
+++ common/js/SubscriberBase.js
@@ -1,8 +1,11 @@
+const { GAME_OVER, END_GAME } = require('common/helpers/commonTopics');
+
 class SubscriberBase {
   constructor(playerId, pubSub) {
     this.playerId = playerId;
     this.pubSub = pubSub;
     this.subscriptions = [];
+    this.mapSubscriptions([GAME_OVER, END_GAME])
   }
 
   mapSubscriptions(topics) {
@@ -13,6 +16,24 @@ class SubscriberBase {
     this.subscriptions.push(this.pubSub.subscribe(topic, this[topic].bind(this)));
   }
 
+  [GAME_OVER]({ id }) {
+    if(this.playerId === id) {
+      this.gameOverAction();
+    }
+  }
+
+  [END_GAME]() {
+    this.endGameAction();
+  }
+
+  gameOverAction() {
+
+  }
+
+  endGameAction() {
+    
+  }
+  
   unsubscribe() {
     this.subscriptions.forEach((unsub) => unsub());
   }
