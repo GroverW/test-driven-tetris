aaa69d1df07e4ac29c31d07eeeb5a574a77043d9
GroverW <cheesemaker@gmail.com>
2020-07-10 11:25:44 -0700

    added jsDocs to Command class

--- frontend/static/js/Command.js
+++ frontend/static/js/Command.js
@@ -1,7 +1,19 @@
 const { publish } = require('frontend/helpers/pubSub');
 const { ADD_TO_QUEUE } = require('frontend/helpers/clientTopics');
 
+
+/**
+ * Represents a game command
+ */
 class Command {
+  /**
+   * Creates a Command
+   * @constructor
+   * @param {number|string} key - keypress identifying command
+   * @param {function} callback - executes the command
+   * @param {boolean|number[]} [toggle] - list of delay timings in ms
+   * @param {number} [delay] - delay in ms
+   */
   constructor(key, callback, toggle=false, delay=0) {
     this.key = key;
     this.type = toggle ? 'toggleCommand' : 'command';
@@ -12,17 +24,26 @@ class Command {
     this.delay = this.getInitialDelay(delay);
   }
 
+  /**
+   * Gets the initial delay in ms
+   * @param {number} delay - delay in ms
+   * @returns {number} - initial delay
+   */
   getInitialDelay(delay) {
     return this.toggle ? this.toggle[this.toggleIdx] : delay;
   }
 
-  execute(time) {
-    if(this.startTime === undefined) this.startTime = time;
+  /**
+   * Handles execution of callback and resetting timers
+   * @param {number} currTime - current game time in ms
+   */
+  execute(currTime) {
+    if(this.startTime === undefined) this.startTime = currTime;
 
-    if(time >= this.startTime + this.delay) {
+    if(currTime >= this.startTime + this.delay) {
       publish(ADD_TO_QUEUE, this.key);
       this.callback();
-      this.startTime = time;
+      this.startTime = currTime;
 
       if(this.toggle) {
         this.handleToggle();
@@ -32,6 +53,9 @@ class Command {
     }
   }
 
+  /**
+   * Increments toggle index and updates delay
+   */
   handleToggle() {
     this.toggleIdx = Math.min(this.toggle.length - 1, this.toggleIdx + 1);
     this.delay = this.toggle[this.toggleIdx];
