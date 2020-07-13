a580490234a95f830d0e7242d5f835a3ecae7efd
GroverW <cheesemaker@gmail.com>
2020-07-12 18:06:53 -0700

    updated ServerBoard class per eslint rules

--- src/js/ServerBoard.js
+++ src/js/ServerBoard.js
@@ -6,15 +6,6 @@ const { GET_PIECES } = require('backend/helpers/serverTopics');
  */
 class ServerBoard extends Board {
   /**
- * @constructor
- * @param {object} pubSub - Publish / Subscribe object
- * @param {number} playerId - Id of player on backend
- */
-  constructor(pubSub, playerId) {
-    super(pubSub, playerId);
-  }
-
-  /**
    * Sets the current and next pieces
    */
   getPieces() {
@@ -24,24 +15,6 @@ class ServerBoard extends Board {
       this.pubSub.publish(GET_PIECES);
     }
   }
-
-  /**
-   * Clears completed lines after piece added to board
-   */
-  clearLines() {
-    const numCleared = super.clearLines();
-    if (numCleared > 0) this.publishBoardUpdate();
-  }
-
-  /**
-   * Publishes any board updates
-   */
-  // publishBoardUpdate() {
-  //   this.pubSub.publish('updateBoard', {
-  //     id: this.id,
-  //     board: this.grid
-  //   })
-  // }
 }
 
 module.exports = ServerBoard;
