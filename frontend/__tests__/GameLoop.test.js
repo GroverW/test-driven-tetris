ed7ca77a9c1e4fba1e15f6c283d56fda8dd040ac
GroverW <cheesemaker@gmail.com>
2020-07-23 19:06:02 -0700

    updated beforeAll to beforeEach

--- frontend/__tests__/GameLoop.test.js
+++ frontend/__tests__/GameLoop.test.js
@@ -1,7 +1,7 @@
 const gameLoop = require('frontend/static/js/GameLoop');
 const Command = require('frontend/static/js/Command');
 const { publish } = require('frontend/helpers/pubSub');
-const { mockAnimation } = require('frontend/mockData/mocks');
+const { mockAnimation, mockCancelAnimation } = require('frontend/mockData/mocks');
 const {
   START_GAME,
   GAME_OVER,
@@ -14,23 +14,23 @@ describe('Game Loop tests', () => {
   let testCommand; let testToggleCommand; let testAutoCommand;
   let testCallback; let testToggleCallback; let testAutoCallback;
 
-  beforeAll(() => {
+  beforeEach(() => {
     gameLoop.initialize(1);
     testCallback = jest.fn();
     testToggleCallback = jest.fn();
     testAutoCallback = jest.fn();
     testCommand = new Command(1, testCallback);
-    testToggleCommand = new Command(2, testToggleCallback, [10, 10]);
+    testToggleCommand = new Command(2, testToggleCallback, [10, 10, 10]);
     testAutoCommand = new Command(false, testAutoCallback, [0, 0, 0]);
     jest.useFakeTimers();
 
     requestAnimationFrame = jest.fn().mockImplementation(mockAnimation());
-    cancelAnimationFrame = jest.fn();
+    cancelAnimationFrame = jest.fn().mockImplementation(mockCancelAnimation);
   });
 
-  afterAll(() => {
+  afterEach(() => {
     jest.clearAllMocks();
-    jest.clearFakeTimers();
+    gameLoop.gameOverAction();
   });
 
   describe('set / clear commands', () => {
@@ -52,12 +52,17 @@ describe('Game Loop tests', () => {
       const newCallback = jest.fn();
       const newCommand = new Command(1, newCallback, [400]);
 
+      gameLoop[SET_COMMAND](testCommand);
       gameLoop[SET_COMMAND](newCommand);
-      expect(testCommand).not.toBe(newCommand);
+
+      expect(gameLoop.command).not.toBe(newCommand);
       expect(gameLoop.command).toBe(testCommand);
     });
 
     test('should clear commands if key matches', () => {
+      gameLoop[SET_COMMAND](testCommand);
+      gameLoop[SET_COMMAND](testToggleCommand);
+
       gameLoop[CLEAR_COMMAND](3);
 
       expect(gameLoop.toggleCommand).toBe(testToggleCommand);
@@ -83,6 +88,11 @@ describe('Game Loop tests', () => {
     });
 
     test('calls commands each frame', () => {
+      gameLoop[SET_COMMAND](testToggleCommand);
+      gameLoop[SET_AUTO_COMMAND](testAutoCommand);
+
+      gameLoop[START_GAME]();
+
       expect(testToggleCallback).toHaveBeenCalledTimes(0);
       expect(testAutoCallback).toHaveBeenCalledTimes(1);
 
@@ -97,6 +107,7 @@ describe('Game Loop tests', () => {
 
       expect(testCallback).toHaveBeenCalledTimes(0);
 
+      gameLoop[START_GAME]();
       jest.advanceTimersByTime(1000);
 
       expect(testCallback).toHaveBeenCalledTimes(1);
@@ -131,7 +142,7 @@ describe('Game Loop tests', () => {
 
     test('SET_COMMAND should set command', () => {
       const newCommand = new Command(0, testCallback);
-      expect(gameLoop.command).toBe(testCommand);
+      expect(gameLoop.command).toBe(undefined);
 
       publish(SET_COMMAND, newCommand);
 
@@ -147,17 +158,19 @@ describe('Game Loop tests', () => {
     test('GAME_OVER should stop animation and unsubscribe if correct playerId', () => {
       const unsubSpy = jest.spyOn(gameLoop, 'unsubscribe');
 
-      expect(cancelAnimationFrame).toHaveBeenCalledTimes(1);
+      gameLoop[START_GAME]();
+
+      expect(cancelAnimationFrame).toHaveBeenCalledTimes(0);
       expect(unsubSpy).toHaveBeenCalledTimes(0);
 
       publish(GAME_OVER, { id: 2 });
 
-      expect(cancelAnimationFrame).toHaveBeenCalledTimes(1);
+      expect(cancelAnimationFrame).toHaveBeenCalledTimes(0);
       expect(unsubSpy).toHaveBeenCalledTimes(0);
 
       publish(GAME_OVER, { id: 1 });
 
-      expect(cancelAnimationFrame).toHaveBeenCalledTimes(2);
+      expect(cancelAnimationFrame).toHaveBeenCalledTimes(1);
       expect(unsubSpy).toHaveBeenCalledTimes(1);
     });
   });
