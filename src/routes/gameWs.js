e3f3bc7773f57f3dfa51d9cb0f4494ac3cbafdc5
GroverW <cheesemaker@gmail.com>
2020-07-30 15:17:10 -0700

    refactored ws into its own file

---
+++ src/routes/gameWs.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const app = express();
+require('express-ws')(app);
+const {
+  getGameById, getNewPlayer, closeConnection, handleMessage, handleClose,
+} = require('backend/helpers/routeHelpers');
+
+const router = express.Router({ mergeParams: true });
+
+router.ws('/:gameId', (ws, req, next) => { // eslint-disable-line consistent-return
+  try {
+    const { gameId } = req.params;
+    const gameServer = getGameById(gameId);
+    const player = getNewPlayer(ws);
+
+    if (!gameServer) closeConnection(ws, 'Game not found.');
+    if (!gameServer.join(player)) closeConnection(ws, 'Could not join game.');
+
+    ws.on('message', handleMessage(player));
+    ws.on('close', handleClose(player));
+  } catch (err) {
+    return next(err);
+  }
+});
+
+module.exports = router;
