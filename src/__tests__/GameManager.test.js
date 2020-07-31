7d78f708cab06bd16108ba93a52d3df66a37c5c3
GroverW <cheesemaker@gmail.com>
2020-07-31 11:28:25 -0700

    adding game start and condition checking to GameManager [WIP]

--- src/__tests__/GameManager.test.js
+++ src/__tests__/GameManager.test.js
@@ -1,13 +1,14 @@
 const GameManager = require('backend/js/GameManager');
+const { GAME_TYPES } = require('common/helpers/constants');
 
 describe('game manager tests', () => {
   let gameManager;
   let p1; let p2;
 
   beforeEach(() => {
-    p1 = jest.fn();
-    p2 = jest.fn();
-    gameManager = new GameManager();
+    p1 = {};
+    p2 = {};
+    gameManager = new GameManager(GAME_TYPES.MULTI);
   });
 
   afterEach(() => {
@@ -49,6 +50,82 @@ describe('game manager tests', () => {
       gameManager.removePlayer(p1);
 
       expect(gameManager.players.length).toBe(0);
+    });
+  });
+
+  describe('start game', () => {
+    describe('check start conditions', () => {
+      beforeEach(() => {
+        gameManager.addPlayer(p1);
+      });
+
+      test('gets total number of players ready', () => {
+        expect(gameManager.getTotalReady()).toBe(0);
+
+        p1.readyToPlay = true;
+
+        expect(gameManager.getTotalReady()).toBe(1);
+      });
+
+      test('returns true if players ready is == total ready', () => {
+        gameManager.addPlayer(p2);
+
+        p1.readyToPlay = true;
+        p2.readyToPlay = true;
+
+        expect(gameManager.checkStartConditions()).toBe(true);
+      });
+
+      test('returns false if players ready is < total ready', () => {
+        gameManager.addPlayer(p2);
+
+        p1.readyToPlay = true;
+
+        expect(gameManager.checkStartConditions()).toBe(false);
+      });
+
+      test('sends error if only one player in multiplayer game', () => {
+        const sendErrorSpy = jest.spyOn(gameManager, 'sendError');
+
+        p1.readyToPlay = true;
+
+        expect(gameManager.checkStartConditions()).toBe(false);
+        expect(sendErrorSpy).toHaveBeenCalledTimes(1);
+        expect(sendErrorSpy).toHaveBeenLastCalledWith(expect.any(String));
+      });
+
+      test('handles single player game', () => {
+        gameManager.gameType = GAME_TYPES.SINGLE;
+
+        expect(gameManager.checkStartConditions()).toBe(false);
+
+        p1.readyToPlay = true;
+
+        expect(gameManager.checkStartConditions()).toBe(true);
+      });
+    });
+
+    describe('player ready', () => {
+      beforeEach(() => {
+        gameManager.addPlayer(p1);
+        gameManager.addPlayer(p2);
+      });
+
+      test('calls animate start if start conditions met', () => {
+        const animateStartSpy = jest.spyOn(gameManager, 'animateStart');
+
+        p1.readyToPlay = true;
+
+        gameManager.playerReady();
+
+        expect(animateStartSpy).toHaveBeenCalledTimes(0);
+
+        p2.readyToPlay = true;
+
+        gameManager.playerReady();
+
+        expect(animateStartSpy).toHaveBeenCalledTimes(1);
+      });
     });
   });
 });
--- src/js/GameManager.js
+++ src/js/GameManager.js
@@ -1,5 +1,8 @@
+const { GAME_TYPES } = require('common/helpers/constants');
+
 class GameManager {
-  constructor() {
+  constructor(gameType) {
+    this.gameType = gameType;
     this.players = [];
   }
 
@@ -11,6 +14,51 @@ class GameManager {
 
   removePlayer(player) {
     this.players = this.players.filter((p) => p !== player);
+  }
+
+  getTotalReady() {
+    return this.players.filter((p) => p.readyToPlay).length;
+  }
+
+  checkStartConditions() {
+    if (this.gameType === GAME_TYPES.MULTI) return this.checkMultiplayerStartConditions();
+    if (this.gameType === GAME_TYPES.SINGLE) return this.checkSingleplayerStartConditions();
+
+    return false;
+  }
+
+  checkMultiplayerStartConditions() {
+    const totalReady = this.getTotalReady();
+    const numPlayers = this.players.length;
+
+    if (totalReady === numPlayers && numPlayers === 1) {
+      this.sendError('Not enough players to start game.');
+      return false;
+    }
+
+    if (totalReady < numPlayers) {
+      return false;
+    }
+
+    return true;
+  }
+
+  checkSingleplayerStartConditions() {
+    return this.getTotalReady() === this.players.length;
+  }
+
+  playerReady() {
+    if (this.checkStartConditions()) {
+      this.animateStart();
+    }
+  }
+
+  animateStart() {
+
+  }
+
+  sendError() {
+
   }
 }
 
