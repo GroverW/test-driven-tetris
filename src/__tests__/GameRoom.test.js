ca08adc5d5b9255d5cd201076c64c7f2c295161c
GroverW <cheesemaker@gmail.com>
2020-08-01 14:05:00 -0700

    added roomAvailable and setPlayerAttributes tests / methods

--- src/__tests__/GameRoom.test.js
+++ src/__tests__/GameRoom.test.js
@@ -5,6 +5,7 @@ const Player = require('backend/js/Player');
 const { mockSend } = require('common/mockData/mocks');
 const pubSub = require('backend/helpers/pubSub');
 const { GAME_TYPES } = require('backend/helpers/serverConstants');
+const { MSG_TYPE } = require('backend/helpers/serverTopics');
 
 describe('game room tests', () => {
   let gameRoom;
@@ -23,6 +24,7 @@ describe('game room tests', () => {
     expect(gameRoom.players).toEqual(expect.any(PlayerManager));
     expect(gameRoom.manager).toEqual(expect.any(GameManager));
     expect(gameRoom.gameType).toBe(GAME_TYPES.MULTI);
+    expect(gameRoom.nextPlayerId).toBe(1);
   });
 
   describe('join room', () => {
@@ -35,18 +37,51 @@ describe('game room tests', () => {
         expect(gameRoom.roomAvailable()).toBe(true);
       });
 
-      test('returns false and sends error if room full', () => {
+      test('returns false and sends error if room full - multiplayer', () => {
+        const sendFlashSpy = jest.spyOn(p5, 'sendFlash');
+        [p2, p3, p4].forEach((p) => gameRoom.players.add(p));
+        expect(gameRoom.roomAvailable(p5)).toBe(false);
+        expect(sendFlashSpy).toHaveBeenLastCalledWith(MSG_TYPE.ERROR, expect.any(String));
+      });
 
+      test('returns false and sends error if room full - single player', () => {
+        const sendFlashSpy = jest.spyOn(p2, 'sendFlash');
+        gameRoom.gameType = GAME_TYPES.SINGLE;
+        expect(gameRoom.roomAvailable(p2)).toBe(false);
+        expect(sendFlashSpy).toHaveBeenLastCalledWith(MSG_TYPE.ERROR, expect.any(String));
       });
 
       test('returns false and sends error if game started', () => {
-
+        const sendFlashSpy = jest.spyOn(p2, 'sendFlash');
+        gameRoom.manager.gameStarted = true;
+        expect(gameRoom.roomAvailable(p2)).toBe(false);
+        expect(sendFlashSpy).toHaveBeenLastCalledWith(MSG_TYPE.ERROR, expect.any(String));
       });
     });
 
     describe('join room steps', () => {
-      test('adds player subscriptions', () => {
+      test('sets player attributes', () => {
+        const setIdSpy = jest.spyOn(p1, 'setId');
+        const setGameTypeSpy = jest.spyOn(p1, 'setGameType');
+        const nextId = gameRoom.nextPlayerId;
 
+        gameRoom.setPlayerAttributes(p1);
+
+        expect(setIdSpy).toHaveBeenLastCalledWith(nextId);
+        expect(setGameTypeSpy).toHaveBeenLastCalledWith(gameRoom.gameType);
+        expect(gameRoom.nextPlayerId).toBeGreaterThan(nextId);
+      });
+
+      test('joining room sets attributes, adds subs, syncs players and returns true', () => {
+        const setPlayerAttributesSpy = jest.spyOn(gameRoom, 'setPlayerAttributes');
+        const addSubscriptionsSpy = jest.spyOn(gameRoom, 'addSubscriptions');
+        const syncPlayersWithSpy = jest.spyOn(gmeRoom, 'syncPlayersWith');
+
+        expect(gameRoom.join(p2)).toBe(true);
+
+        expect(setPlayerAttributesSpy).toHaveBeenCalledTimes(1);
+        expect(addSubscriptionsSpy).toHaveBeenCalledTimes(1);
+        expect(syncPlayersWithSpy).toHaveBeenCalledTimes(1);
       });
 
       test('returns true if check room conditions met', () => {
--- src/js/GameRoom.js
+++ src/js/GameRoom.js
@@ -1,15 +1,71 @@
 const PlayerManager = require('backend/js/PlayerManager');
 const GameManager = require('backend/js/GameManager');
+const { MAX_PLAYERS } = require('backend/helpers/serverConstants');
+const { MSG_TYPE } = require('backend/helpers/serverTopics');
 
 class GameRoom {
   constructor(gameType) {
     this.gameType = gameType;
     this.players = new PlayerManager();
     this.manager = new GameManager(gameType, this.players);
+    this.nextPlayerId = 1;
   }
 
-  roomAvailable() {
+  roomAvailable(player) {
+    if (this.roomIsFull()) {
+      player.sendFlash(MSG_TYPE.ERROR, 'That game is full.')
+      return false;
+    }
+
+    if (this.manager.gameStarted) {
+      player.sendFlash(MSG_TYPE.ERROR, 'That game has already started.');
+      return false;
+    }
+
     return true;
+  }
+
+  roomIsFull() {
+    return this.players.count >= MAX_PLAYERS[this.gameType];
+  }
+
+  addToRoom(player) {
+    if (!this.players.add(player)) {
+      player.sendFlash(MSG_TYPE.ERROR, 'Somehow you\'re already in the room...');
+      return false;
+    }
+
+    return true;
+  }
+
+  setPlayerAttributes(player) {
+    player.setId(this.nextPlayerId);
+    player.setGameType(this.gameType);
+    this.nextPlayerId += 1;
+  }
+
+  join(player) {
+    // if (!this.checkGameStatus(player)) return false;
+    // if (this.players.includes(player)) return false;
+
+    // this.nextPlayerId += 1;
+    // player.setId(this.nextPlayerId);
+    // player.setGameType(this.gameType);
+
+    // this.players.push(player);
+    // this.addSubscriptions(player);
+
+    // // send new player to existing players
+    // this.sendAll({ type: ADD_PLAYER, data: player.id });
+
+    // // send existing players to new player
+    // this.players.forEach((p) => {
+    //   if (p !== player) player.sendMessage({ type: ADD_PLAYER, data: p.id });
+    // });
+
+    // this.setHostOnJoin();
+
+    // return true;
   }
 }
 
