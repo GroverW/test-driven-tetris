54ed96cd2d343dda0f494fb47392a0c9d94347ef
GroverW <cheesemaker@gmail.com>
2020-07-31 18:01:13 -0700

    added setup, start steps, getPieces, startPlayerGames tests [WIP]

--- src/__tests__/GameManager.test.js
+++ src/__tests__/GameManager.test.js
@@ -1,18 +1,35 @@
 const GameManager = require('backend/js/GameManager');
-const { GAME_TYPES } = require('common/helpers/constants');
+const MessageManager = require('backend/js/MessageManager');
+const { GAME_TYPES, COUNTDOWN } = require('backend/helpers/serverConstants');
+const { START_GAME, ADD_PIECES } = require('backend/helpers/serverTopics');
+const pubSub = require('backend/helpers/pubSub');
+const { pubSubMock } = require('common/mockData/mocks');
 
 describe('game manager tests', () => {
   let gameManager;
   let p1; let p2;
+  let pubSubSpy;
+
+  const getPlayer = (id) => ({
+    id,
+    addPieces: jest.fn(),
+    game: { start: jest.fn() },
+  });
 
   beforeEach(() => {
-    p1 = {};
-    p2 = {};
-    gameManager = new GameManager(GAME_TYPES.MULTI);
+    const serverPubSub = pubSub();
+    pubSubSpy = pubSubMock(serverPubSub);
+    p1 = getPlayer(1);
+    p2 = getPlayer(2);
+    gameManager = new GameManager(GAME_TYPES.MULTI, serverPubSub);
   });
 
   afterEach(() => {
     jest.clearAllMocks();
+  });
+
+  test('setup', () => {
+    expect(gameManager.msg).toEqual(expect.any(MessageManager));
   });
 
   describe('add / remove players', () => {
@@ -111,6 +128,35 @@ describe('game manager tests', () => {
         gameManager.addPlayer(p2);
       });
 
+      test('goes through start steps', () => {
+        const startSpy = pubSubSpy.add(START_GAME);
+        const getPiecesSpy = jest.spyOn(gameManager, 'getPieces');
+        const startPlayerGamesSpy = jest.spyOn(gameManager, 'startPlayerGames');
+        const sendAllSpy = jest.spyOn(gameManager.msg, 'sendAll');
+        const updateRankingsSpy = jest.spyOn(gameManager, 'updateRankings');
+
+        gameManager.startGame();
+
+        expect(startSpy).toHaveBeenCalledTimes(1);
+        expect(getPiecesSpy).toHaveBeenCalledTimes(1);
+        expect(startPlayerGamesSpy).toHaveBeenCalledTimes(1);
+        expect(sendAllSpy).toHaveBeenCalledTimes(2);
+        expect(updateRankingsSpy).toHaveBeenCalledTimes(1);
+      });
+
+      test('game start animates start', () => {
+        jest.useFakeTimers();
+        const sendGameMessageSpy = jest.spyOn(gameManager.msg, 'sendGameMessage');
+        const startGameSpy = jest.spyOn(gameManager, 'startGame');
+
+        gameManager.animateStart();
+
+        jest.advanceTimersByTime(COUNTDOWN.NUM_INTERVALS * COUNTDOWN.INTERVAL_LENGTH);
+
+        expect(sendGameMessageSpy).toHaveBeenCalledTimes(COUNTDOWN.NUM_INTERVALS);
+        expect(startGameSpy).toHaveBeenCalledTimes(1);
+      });
+
       test('calls animate start if start conditions met', () => {
         const animateStartSpy = jest.spyOn(gameManager, 'animateStart');
 
@@ -125,6 +171,43 @@ describe('game manager tests', () => {
         gameManager.playerReady();
 
         expect(animateStartSpy).toHaveBeenCalledTimes(1);
+      });
+    });
+  });
+
+  describe('game actions', () => {
+    beforeEach(() => {
+      gameManager.addPlayer(p1);
+      gameManager.addPlayer(p2);
+    });
+
+    test('getPieces adds a new set of pieces for each player', () => {
+      const sendAllSpy = jest.spyOn(gameManager.msg, 'sendAll');
+
+      gameManager.getPieces();
+
+      expect(p1.addPieces).toHaveBeenLastCalledWith(expect.any(Array));
+      expect(p2.addPieces).toHaveBeenLastCalledWith(expect.any(Array));
+      expect(sendAllSpy).toHaveBeenLastCalledWith({
+        type: ADD_PIECES,
+        data: expect.any(Array),
+      });
+    });
+
+    test('startPlayerGames starts each players games', () => {
+      gameManager.startPlayerGames();
+
+      expect(p1.game.start).toHaveBeenCalledTimes(1);
+      expect(p2.game.start).toHaveBeenCalledTimes(1);
+    });
+
+    describe('update rankings', () => {
+      test('sets ranking if no rankings', () => {
+
+      });
+
+      test('updates rankings if rankings exist', () => {
+
       });
     });
   });
