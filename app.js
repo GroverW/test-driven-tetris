253d91abb3275424045b4c9f292325deeae23862
GroverW <cheesemaker@gmail.com>
2020-08-02 16:14:38 -0700

    refactored websocket to own route file

--- app.js
+++ app.js
@@ -5,26 +5,11 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const games = require('backend/routes/games');
-const {
-  getGameById, getNewPlayer, closeConnection, handleMessage, handleClose, createGame,
-} = require('backend/helpers/routeHelpers');
-// const gameWs = require('backend/routes/gameWs');
 
 app.use(express.static('frontend/static/'));
 
 app.use('/games', games);
-// app.use('/game', gameWs);
-
-io.of('/game').on('connection', (socket) => {
-  const { gameId } = socket.handshake.query;
-  socket.join(gameId);
-  const gameRoom = getGameById(gameId);
-  const player = getNewPlayer(socket);
-  gameRoom.join(player);
-
-  socket.on('message', handleMessage(player));
-  socket.on('disconnect', handleClose(player));
-});
+require('backend/routes/gameWs')(io);
 
 app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
   res.status(err.status || 500);
