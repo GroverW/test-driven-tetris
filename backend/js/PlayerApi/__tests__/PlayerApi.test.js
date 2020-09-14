2da347a473b683a8c0acf9f9f1a9e9f096792f95
GroverW <cheesemaker@gmail.com>
2020-09-14 13:56:00 -0700

    setup PlayerApi test skeleton

---
+++ backend/js/PlayerApi/__tests__/PlayerApi.test.js
@@ -0,0 +1,31 @@
+const PlayerApi = require('..');
+
+describe('player api tests', () => {
+  let api;
+
+  beforeEach(() => {
+    api = new PlayerApi();
+  });
+
+  describe('setup', () => {
+    test('create new PlayerApi', () => {
+
+    });
+  });
+
+  describe('join / leave games', () => {
+
+  });
+
+  describe('handle actions', () => {
+    test('handle close', () => {
+
+    });
+
+    describe('handle message', () => {
+      test('create new PlayerApi', () => {
+
+      });
+    });
+  });
+});
