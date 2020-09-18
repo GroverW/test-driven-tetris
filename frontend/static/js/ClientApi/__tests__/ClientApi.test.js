dba41bafbe3ced1942859d7d1fdc5e9b373d6287
GroverW <cheesemaker@gmail.com>
2020-09-18 16:29:51 -0700

    added tests / methods for creating / joining games

--- frontend/static/js/ClientApi/__tests__/ClientApi.test.js
+++ frontend/static/js/ClientApi/__tests__/ClientApi.test.js
@@ -1,15 +1,18 @@
 const ClientApi = require('frontend/static/js/ClientApi');
 const { formatMessage } = require('frontend/helpers/utils');
 const { pubSubMock } = require('frontend/mocks');
-const { ADD_MESSAGE } = require('frontend/topics');
+const { ADD_MESSAGE, CREATE_GAME, JOIN_GAME } = require('frontend/topics');
+const { GAME_TYPES } = require('frontend/constants');
 
 describe('client api tests', () => {
-  const mockWs = { send: jest.fn() };
+  const mockWs = () => ({ send: jest.fn() });
+  let ws;
   let api;
   let pubSubSpy;
 
   beforeEach(() => {
-    api = new ClientApi(mockWs);
+    ws = mockWs();
+    api = new ClientApi(ws);
     pubSubSpy = pubSubMock();
   });
 
@@ -19,7 +22,7 @@ describe('client api tests', () => {
 
   describe('setup', () => {
     test('creates new ClientApi with access to websocket', () => {
-      expect(api.ws).toBe(mockWs);
+      expect(api.ws).toBe(ws);
     });
   });
 
@@ -45,11 +48,20 @@ describe('client api tests', () => {
 
   describe('join / create game', () => {
     test('sends correct create message', () => {
+      const message = formatMessage({ type: CREATE_GAME, data: GAME_TYPES.MULTI });
 
+      api.createGame(GAME_TYPES.MULTI);
+
+      expect(api.ws.send).toHaveBeenLastCalledWith(message);
     });
 
     test('sends correct join message', () => {
+      const gameId = 'test';
+      const message = formatMessage({ type: JOIN_GAME, data: gameId });
 
+      api.joinGame(gameId);
+
+      expect(api.ws.send).toHaveBeenLastCalledWith(message);
     });
   });
 
--- frontend/static/js/ClientApi/index.js
+++ frontend/static/js/ClientApi/index.js
@@ -1,4 +1,5 @@
 const { publishError, formatMessage } = require('frontend/helpers/utils');
+const { CREATE_GAME, JOIN_GAME } = require('frontend/topics');
 
 class ClientApi {
   constructor(ws) {
@@ -11,6 +12,14 @@ class ClientApi {
     } catch (err) {
       publishError('Could not send message to backend.');
     }
+  }
+
+  createGame(gameType) {
+    this.sendMessage({ type: CREATE_GAME, data: gameType });
+  }
+
+  joinGame(gameId) {
+    this.sendMessage({ type: JOIN_GAME, data: gameId });
   }
 }
 
