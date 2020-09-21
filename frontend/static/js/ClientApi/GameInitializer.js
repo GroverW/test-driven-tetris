c0868996444ea1bcea0e67c1cb907946c1899571
GroverW <cheesemaker@gmail.com>
2020-09-21 15:15:25 -0700

    added keyboard command and setup tests / methods for GameInitializer

--- frontend/static/js/ClientApi/GameInitializer.js
+++ frontend/static/js/ClientApi/GameInitializer.js
@@ -1,6 +1,15 @@
+const ClientGame = require('frontend/static/js/ClientGame');
+const { createNullObject } = require('frontend/helpers/utils');
+const { createGameEventListeners } = require('./helpers');
+
 class GameInitializer {
   constructor() {
+    this.currentGame = createNullObject(ClientGame);
+    createGameEventListeners(this);
+  }
 
+  command(key, keyDirection) {
+    this.currentGame.command(key, keyDirection);
   }
 }
 
--- frontend/static/js/ClientApi/__tests__/GameInitializer.test.js
+++ frontend/static/js/ClientApi/__tests__/GameInitializer.test.js
@@ -1,5 +1,11 @@
-const GameInitializer = require('frontend/static/js/ClientApi/GameInitializer');
 const ClientGame = require('frontend/static/js/ClientGame');
+const { createNullObject } = require('frontend/helpers/utils');
+const { getMockDOMSelector } = require('frontend/mocks');
+
+document.querySelector = jest.fn().mockImplementation(getMockDOMSelector);
+document.getElementById = jest.fn().mockImplementation(getMockDOMSelector);
+
+const GameInitializer = require('frontend/static/js/ClientApi/GameInitializer');
 
 describe('game initializer tests', () => {
   let gameInitializer;
@@ -8,12 +14,46 @@ describe('game initializer tests', () => {
     gameInitializer = new GameInitializer();
   });
 
-  test('check clientgame keys', () => {
-    const test = {}
-    Object.getOwnPropertyNames(ClientGame.prototype).forEach((propertyName) => {
-      test[propertyName] = () => {};
-    });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
-    console.log(test);
-  })
+  describe('setup', () => {
+    test('expect current game to be a null object, event listeners to be setup', () => {
+      Object.getOwnPropertyNames(ClientGame.prototype)
+        .forEach((propertyName) => {
+          expect(gameInitializer.currentGame[propertyName]).not.toBe(undefined);
+        });
+    });
+  });
+
+  describe('keyboard commands', () => {
+    test('keyboard events should trigger commands for the current game', () => {
+      const game1 = gameInitializer.currentGame;
+      const game2 = createNullObject(ClientGame);
+      const command1Spy = jest.spyOn(game1, 'command');
+      const command2Spy = jest.spyOn(game2, 'command');
+
+      expect(command1Spy).toHaveBeenCalledTimes(0);
+      expect(command2Spy).toHaveBeenCalledTimes(0);
+
+      document.dispatchEvent(new KeyboardEvent('keydown'));
+
+      expect(command1Spy).toHaveBeenCalledTimes(1);
+      expect(command2Spy).toHaveBeenCalledTimes(0);
+
+      gameInitializer.currentGame = game2;
+
+      document.dispatchEvent(new KeyboardEvent('keydown'));
+
+      expect(command1Spy).toHaveBeenCalledTimes(1);
+      expect(command2Spy).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('initialize', () => {
+    test('initialize creates new client game, ', () => {
+
+    });
+  });
 });
