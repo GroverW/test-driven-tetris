8157c0e3a130a0d4561b31764f8fb5faa4fb7612
GroverW <cheesemaker@gmail.com>
2020-09-25 14:28:40 -0700

    added tests / method for ADD_POWER_UP to PowerUpDisplay

--- frontend/static/js/GameDOM/PowerUpDisplay.js
+++ frontend/static/js/GameDOM/PowerUpDisplay.js
@@ -1,8 +1,37 @@
-const { mapPowerUps } = require('frontend/static/js/GameDOM/DOMHelpers');
+const SubscriberBase = require('common/js/SubscriberBase');
+const pubSub = require('frontend/helpers/pubSub');
+const {
+  mapPowerUps, getPowerUpType, getNullPowerUp,
+} = require('frontend/static/js/GameDOM/DOMHelpers');
+const { ADD_POWER_UP } = require('frontend/topics');
 
-class PowerUpDisplay {
+class PowerUpDisplay extends SubscriberBase {
   constructor(selectors) {
+    super(pubSub, null);
     this.powerUps = mapPowerUps(selectors);
+    this.nextIdx = 0;
+    this.mapSubscriptions([ADD_POWER_UP]);
+  }
+
+  [ADD_POWER_UP](powerUp) {
+    const nextPowerUpDisplay = this.getNextPowerUpDisplay();
+    const [powerUpType, powerUpClass] = getPowerUpType(powerUp);
+    nextPowerUpDisplay.type = powerUpType;
+    nextPowerUpDisplay.node.classList.add(powerUpClass);
+    this.incrementNextIdx(nextPowerUpDisplay);
+  }
+
+  getNextPowerUpDisplay() {
+    const nextPowerUpDisplay = this.powerUps[this.nextIdx] || getNullPowerUp();
+    return nextPowerUpDisplay;
+  }
+
+  incrementNextIdx(currentPowerUpDisplay) {
+    const increment = currentPowerUpDisplay.type !== null;
+    this.nextIdx = Math.min(
+      this.powerUps.length,
+      this.nextIdx + increment,
+    );
   }
 }
 
--- frontend/static/js/GameDOM/__tests__/PowerUpDisplay.test.js
+++ frontend/static/js/GameDOM/__tests__/PowerUpDisplay.test.js
@@ -1,5 +1,7 @@
 const PowerUpDisplay = require('frontend/static/js/GameDOM/PowerUpDisplay');
-const { MAX_POWER_UPS } = require('frontend/constants');
+const { publish } = require('frontend/helpers/pubSub');
+const { MAX_POWER_UPS, POWER_UP_TYPES } = require('frontend/constants');
+const { ADD_POWER_UP } = require('frontend/topics');
 const { getMockGameDOMSelectors } = require('frontend/mocks');
 
 describe('power up display tests', () => {
@@ -20,12 +22,33 @@ describe('power up display tests', () => {
   });
 
   describe('add / use power up', () => {
-    test('adds power up if valid type', () => {
+    const numPowerUps = (powerUps) => powerUps.filter((p) => p.type !== null).length;
 
+    test('adds power up if valid type', () => {
+      expect(powerUpDisplay.nextIdx).toBe(0);
+      publish(ADD_POWER_UP, POWER_UP_TYPES.SWAP_LINES);
+
+      expect(numPowerUps(powerUpDisplay.powerUps)).toBe(1);
+      expect(powerUpDisplay.nextIdx).toBe(1);
+
+      publish(ADD_POWER_UP, -5);
+
+      expect(numPowerUps(powerUpDisplay.powerUps)).toBe(1);
+      expect(powerUpDisplay.nextIdx).toBe(1);
+
+      publish(ADD_POWER_UP, POWER_UP_TYPES.SCRAMBLE_BOARD);
+
+      expect(numPowerUps(powerUpDisplay.powerUps)).toBe(2);
+      expect(powerUpDisplay.nextIdx).toBe(2);
     });
 
-    test('does not add power up if invalid type', () => {
+    test('does not add more than MAX_POWER_UPS', () => {
+      ['SWAP_LINES', 'SCRAMBLE_BOARD', 'SWAP_LINES', 'SCRAMBLE_BOARD'].forEach((type) => {
+        publish(ADD_POWER_UP, POWER_UP_TYPES[type]);
+      });
 
+      expect(powerUpDisplay.nextIdx).toBe(2);
+      expect(numPowerUps(powerUpDisplay.powerUps)).toBe(2);
     });
 
     test('uses first power up and sets first to second', () => {
